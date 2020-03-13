81aef0220e3574166c934f8cfe083b9639d5774b
SofianSaleh <sofiane.saleh11@egmail.com>
2020-03-13 11:14:44 +0100

    codewars

--- JavaScript/DifferentiatePolynomial.js
+++ JavaScript/DifferentiatePolynomial.js
@@ -38,28 +38,25 @@ function differentiate(equation, point) {
     let result = ''
     for (let j = 0; j < arr.length; j++) {
         if (arr[j].includes('^')) {
-            result += `${differentiateType2(arr[j])}${!!signs[j]?signs[j]:''}`
-        }else if (!arr[j].includes('x')){
-            result = result.slice(0,result.length - 1)
+            result += `${differentiateType2(arr[j])}${!!signs[j] ? signs[j] : ''}`
+        } else if (!arr[j].includes('x')) {
+            result = result.slice(0, result.length - 1)
             continue
         } else {
             result += `${differentiateType1(arr[j])}${signs[j]}`
         }
 
     }
-    console.log(result)
     let result1 = ''
     for (let k = 0; k < result.length; k++) {
-        if(result[k] === 'x'){
-            result1+= `*(${point})`
-            console.log(result[k])
-        }else if(result[k] === "^"){
-            result1+= `**`
-        }else{
-            result1+= result[k]
-
+        if (result[k] === 'x') {
+            result1 += `*(${point})`
+        } else if (result[k] === "^") {
+            result1 += `**`
+        } else {
+            result1 += result[k]
         }
-        
+
     }
     console.log(result1)
     let resut = eval(result1)
@@ -69,29 +66,35 @@ function differentiate(equation, point) {
 
 
 function differentiateType1(str) {
-    let res = str.length > 1 ? str.split('x').join('') :  1
+    if (str.length == 2 && str[0] === '-') {
+        return '-1'
+    }
+    let res = str.length > 1 ? str.split('x').join('') : 1
     return res
 
 }
 function differentiateType2(str) {
+
     let string = str.split('^')
     // console.log(parseInt(string[0]))
-    if(string[0].length === 1){
+    if (string[0].length === 1) {
         string[0] = `${parseInt(string[1])}${string[0]}`
-    }else{
+    } else {
         let sub = string[0].split('x')
+        if (sub[0] === '-') {
+            sub[0] = '-1'
+        }
         sub[0] = parseInt(string[1]) * parseInt(sub[0])
         string[0] = `${sub[0]}x`
     }
-    
+
     string[1] = parseInt(string[1]) - 1
     return string[1] > 1 ? string.join('^') : string[0]
 
 }
-// console.log(differentiateType1("12x", 3))
-// console.log(differentiateType2("100x^3"))
 
-// console.log(differentiate("12x+2", 3))
-  console.log(differentiate("-7x^5+22x^4-55x^3-94x^2+87x-56", -3))
-//   console.log(differentiate("-5x^2+10x+4", 3))
-//   console.log(differentiate("x-66", 3))
+
+console.log(differentiate("12x+2", 3))
+console.log(differentiate("-x^2+3x-3", 1234567908))
+console.log(differentiate("-5x^2+10x+4", 3))
+console.log(differentiate("x-66", 3))
