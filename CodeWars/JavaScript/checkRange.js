b036b286cb4097957340c57d8759c8552b58f348
SofianSaleh <sofiane.saleh11@gmail.com>
2020-07-09 14:25:09 +0200

    :rocket: done

--- CodeWars/JavaScript/checkRange.js
+++ CodeWars/JavaScript/checkRange.js
@@ -18,4 +18,3 @@
 checkRange = (a, x, y) => a.map(v => i += x > v == v > y, i = 0) | i;
 //coding and coding.. shorter and more shorter..  good luck! ;-)
 console.log(checkRange([2, 5, 6, 7, 1, 3, 4, 11, 56, 49], 1, 7), 7);
-)
