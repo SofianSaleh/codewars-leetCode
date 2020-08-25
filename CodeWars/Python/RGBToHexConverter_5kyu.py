6c31627d47946686b73f516f984b62509e4d62e0
SofianSaleh <sofiane.saleh11@gmail.com>
2020-08-25 14:08:57 +0200

    :fire: boilerplate

--- CodeWars/Python/RGBToHexConverter_5kyu.py
+++ CodeWars/Python/RGBToHexConverter_5kyu.py
@@ -4,5 +4,31 @@ words = {"10": 'A', "11": 'B', "12": 'C', "13": 'D', "14": 'E', "15": 'F'}
 def rgb(r, g, b):
     arrOfRGB = [r, g, b]
     hex = ''
+    for x in arrOfRGB:
+        if x < 0:
+            hex += '00'
+        elif x > 255:
+            hex += "FF"
+        else:
+            og = x / 16
+            first = int(og)
+#         if first < 0:
+#             first *=-1
+            second = int((og - first) * 16)
+#         if(second < 0):
+#             second *= -1
+
+            if str(first) in words:
+
+                hex += words[str(first)]
+
+            else:
+                hex += str(first)
+            if str(second) in words:
+
+                hex += words[str(second)]
+
+            else:
+                hex += str(second)
 
     return hex
