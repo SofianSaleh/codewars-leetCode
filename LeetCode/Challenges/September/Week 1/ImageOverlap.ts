1102be4c3bcf89286e034257111f5c2f45155e5d
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-01 12:44:21 +0200

    :rocket: done

--- LeetCode/Challenges/September/Week 1/ImageOverlap.ts
+++ LeetCode/Challenges/September/Week 1/ImageOverlap.ts
@@ -1,39 +1,90 @@
 var largestOverlap = function (A: number[][], B: number[][]) {
-    let LA: any = []
-    let LB: any = []
-    let count = {}
-    let N: number = A.length
+  let LA: any = [];
+  let LB: any = [];
+  let count = {};
+  let N: number = A.length;
 
-    for (let i: number = 0; i < N; i++) {
-        for (let j: number = 0; j < N; j++) {
-            if (A[i][j] === 1) {
-                LA.push([i, j])
-            }
-            if (B[i][j] === 1) {
-                LB.push([i, j])
-            }
-        }
-
+  for (let i: number = 0; i < N; i++) {
+    for (let j: number = 0; j < N; j++) {
+      if (A[i][j] === 1) {
+        LA.push([i, j]);
+      }
+      if (B[i][j] === 1) {
+        LB.push([i, j]);
+      }
     }
-    // dict[(a[0] - b[0]) * 100 + (a[1] - b[1])] += 1
-    // dict[(a[0] - b[0]) * magic + (a[1] - b[1])] += 1
-    for (const a of LA) {
-        for (const b of LB) {
-            let res: number = (a[0] - b[0]) * 100 + (a[1] - b[1])
-            if (!count[res]) {
-                count[(a[0] - b[0]) * 100 + (a[1] - b[1])] = 1
-            } else {
-                count[(a[0] - b[0]) * 100 + (a[1] - b[1])] += 1
-
-            }
-        }
+  }
+  // dict[(a[0] - b[0]) * 100 + (a[1] - b[1])] += 1
+  // dict[(a[0] - b[0]) * magic + (a[1] - b[1])] += 1
+  for (const a of LA) {
+    for (const b of LB) {
+      let res: number = (a[0] - b[0]) * 100 + (a[1] - b[1]);
+      if (!count[res]) {
+        count[(a[0] - b[0]) * 100 + (a[1] - b[1])] = 1;
+      } else {
+        count[(a[0] - b[0]) * 100 + (a[1] - b[1])] += 1;
+      }
     }
+  }
 
-    let values: any = Object.keys(count).map(key => count[key]);
+  let values: any = Object.keys(count).map((key) => count[key]);
 
-
-    return !!count ? Math.max(...values) : 0
-
+  return !!count ? Math.max(...values) : 0;
 };
 
-console.log(largestOverlap([[1, 1, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 0], [0, 1, 1], [0, 0, 1]]))
+console.log(
+  largestOverlap(
+    [
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 1, 0],
+    ],
+    [
+      [0, 0, 0],
+      [0, 1, 1],
+      [0, 0, 1],
+    ]
+  )
+);
+console.log(
+  largestOverlap(
+    [
+      [1, 1, 1],
+      [0, 1, 0],
+      [0, 1, 0],
+    ],
+    [
+      [0, 0, 0],
+      [0, 1, 1],
+      [0, 0, 1],
+    ]
+  )
+);
+console.log(
+  largestOverlap(
+    [
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 1, 0],
+    ],
+    [
+      [0, 0, 0],
+      [0, 1, 1],
+      [0, 1, 1],
+    ]
+  )
+);
+console.log(
+  largestOverlap(
+    [
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 1, 0],
+    ],
+    [
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 0, 1],
+    ]
+  )
+);
