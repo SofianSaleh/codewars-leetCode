5f3f576dfaf7b0d91eaa35a2f087907db3afd677
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-01 12:43:41 +0200

    :rocket: done

--- LeetCode/Challenges/September/Week 1/ContainsDuplicateIII.ts
+++ LeetCode/Challenges/September/Week 1/ContainsDuplicateIII.ts
@@ -1,16 +1,21 @@
-const containsNearbyAlmostDuplicate = (nums: number[], k: number, t: number): boolean => {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (!((j - i) > k)) {
-                if (Math.abs(nums[i] - nums[j]) <= t) {
-                    return true
-                }
-            }
-
+const containsNearbyAlmostDuplicate = (
+  nums: number[],
+  k: number,
+  t: number
+): boolean => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (!(j - i > k)) {
+        if (Math.abs(nums[i] - nums[j]) <= t) {
+          return true;
         }
+      }
     }
-    return false
+  }
+  return false;
 };
 
-console.log(containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3))
-console.log(containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0))
+console.log(containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3));
+console.log(containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0));
+console.log(containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3));
+console.log(containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0));
