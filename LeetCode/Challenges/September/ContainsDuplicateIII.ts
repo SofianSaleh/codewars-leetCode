0b41c9b14cc464bed582457695f96079f15bd36b
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-02 21:20:47 +0200

    :fire: tying

--- LeetCode/Challenges/September/ContainsDuplicateIII.ts
+++ LeetCode/Challenges/September/ContainsDuplicateIII.ts
@@ -1,10 +1,8 @@
-function containsNearbyAlmostDuplicate(nums: number[], k: number, t: number): boolean {
+const containsNearbyAlmostDuplicate = (nums: number[], k: number, t: number): boolean => {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            console.log(j, i)
             if (!((j - i) > k)) {
-                let sum: number = Math.abs(nums[i] - nums[j])
-                if (sum <= t) {
+                if (Math.abs(nums[i] - nums[j]) <= t) {
                     return true
                 }
             }
