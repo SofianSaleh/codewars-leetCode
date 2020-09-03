83b81d396d8d520754ea94e42a10013c6516c3d2
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-03 10:52:07 +0200

    :fire: trying

--- LeetCode/Challenges/September/RepeatedSubstringPattern.ts
+++ LeetCode/Challenges/September/RepeatedSubstringPattern.ts
@@ -1,7 +1,8 @@
 function repeatedSubstringPattern(s: string): boolean {
     let len: number = s.length
+    if (len === 1) return false
 
-    for (let i: number = len / 2; i >= 1; i--) {
+    for (let i: number = Math.round(len / 2); i >= 1; i--) {
         if (len % i === 0) {
             let repeat = len / i
             let substring: string = s.slice(0, i)
@@ -16,6 +17,7 @@ function repeatedSubstringPattern(s: string): boolean {
     return false
 };
 
-console.log(repeatedSubstringPattern("abab"))   // true
-console.log(repeatedSubstringPattern("aba"))    // false
-console.log(repeatedSubstringPattern("abcabcabcabc"))   // true
+// console.log(repeatedSubstringPattern("abab"))   // true
+// console.log(repeatedSubstringPattern("aba"))    // false
+// console.log(repeatedSubstringPattern("abcabcabcabc"))   // true
+console.log(repeatedSubstringPattern("babbabbabbabbab"))   // true
