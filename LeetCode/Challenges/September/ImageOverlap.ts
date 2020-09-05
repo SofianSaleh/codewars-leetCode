b04371cd956ff89c58c9ef71f61462adb5e37023
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-05 06:48:48 +0200

    :fire: trying

--- LeetCode/Challenges/September/ImageOverlap.ts
+++ LeetCode/Challenges/September/ImageOverlap.ts
@@ -30,36 +30,10 @@ var largestOverlap = function (A: number[][], B: number[][]) {
     }
 
     let values: any = Object.keys(count).map(key => count[key]);
-    console.log(values, !!count)
-    if (!!count) {
-        console.log(Math.max(...values))
-        return Math.max(...values)
-    } else {
-        return 0
-    }
-    // return !!count ? Math.max(values) : 0
+
+
+    return !!count ? Math.max(...values) : 0
 
 };
 
 console.log(largestOverlap([[1, 1, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 0], [0, 1, 1], [0, 0, 1]]))
-// class Solution:
-//     def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
-//         n = len(A)
-//         magic = 100
-
-//         onesA = []
-//         onesB = []
-//         dict = collections.defaultdict(int)
-
-//         for i in range(n):
-//             for j in range(n):
-//                 if A[i][j] == 1:
-//                     onesA.append([i, j])
-//                 if B[i][j] == 1:
-//                     onesB.append([i, j])
-
-//         for a in onesA:
-//             for b in onesB:
-//                 
-
-//         return max(dict.values()) if dict else 0
