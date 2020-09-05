b9c5a70cf3e3da59ff821334475cd8e1ce4a959a
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-05 04:43:04 +0200

    :fire:

---
+++ LeetCode/Challenges/September/AllElementsInBST.ts
@@ -0,0 +1,33 @@
+
+//  // Definition for a binary tree node.
+//  class TreeNode {
+//      val: number
+//      left: TreeNode | null
+//      right: TreeNode | null
+//      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//          this.val = (val===undefined ? 0 : val)
+//          this.left = (left===undefined ? null : left)
+//          this.right = (right===undefined ? null : right)
+//      }
+//  }
+
+
+function getAllElements(root1: TreeNode | null, root2: TreeNode | null): number[] {
+    let result: number[] = []
+    function traverse(node:any):any {
+        if(!node){
+            return []
+        }
+        return [node.val] + traverse(node.left) + traverse(node.right)
+        
+    }
+    let res:any = traverse(root1)
+    res += traverse(root2)
+    for(let i = 0; i < res.length; i++) {
+        if(res[i] !== null) {
+            result.push(parseInt(res[i]))
+        }
+    }
+    console.log(result)
+    return result.sort((a,b)=> a-b)
+};
