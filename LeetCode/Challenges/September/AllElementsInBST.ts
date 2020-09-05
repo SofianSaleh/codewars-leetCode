527014dca2345ee69d460dd498760906b88802ad
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-05 04:56:24 +0200

    :fire: trying

--- LeetCode/Challenges/September/AllElementsInBST.ts
+++ LeetCode/Challenges/September/AllElementsInBST.ts
@@ -1,33 +1,39 @@
 
-//  // Definition for a binary tree node.
-//  class TreeNode {
-//      val: number
-//      left: TreeNode | null
-//      right: TreeNode | null
-//      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-//          this.val = (val===undefined ? 0 : val)
-//          this.left = (left===undefined ? null : left)
-//          this.right = (right===undefined ? null : right)
-//      }
-//  }
+// Definition for a binary tree node.
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
 
 
 function getAllElements(root1: TreeNode | null, root2: TreeNode | null): number[] {
     let result: number[] = []
-    function traverse(node:any):any {
-        if(!node){
+
+    function traverse(node: TreeNode): any {
+        if (!node) {
             return []
         }
-        return [node.val] + traverse(node.left) + traverse(node.right)
-        
+        return traverse(node.left) + " " + [node.val] + " " + traverse(node.right)
     }
-    let res:any = traverse(root1)
-    res += traverse(root2)
-    for(let i = 0; i < res.length; i++) {
-        if(res[i] !== null) {
-            result.push(parseInt(res[i]))
+
+    let res: any = traverse(root1) + " " + traverse(root2)
+    res = res.split(' ')
+
+    for (let i = 0; i < res.length; i++) {
+        console.log(res[i] !== 'NaN', res[i] === '', res[i])
+        if (res[i] !== 'NaN') {
+            if (res[i] !== '') {
+                result.push(parseInt(res[i]))
+            }
         }
     }
-    console.log(result)
-    return result.sort((a,b)=> a-b)
+
+    return result.sort((a, b) => a - b)
 };
