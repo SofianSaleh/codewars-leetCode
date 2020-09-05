2b65f987a71a6fc39fd5186489bb4180989252d0
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-05 06:48:27 +0200

    :fire: trying

--- LeetCode/Challenges/September/ImageOverlap.ts
+++ LeetCode/Challenges/September/ImageOverlap.ts
@@ -1,26 +1,65 @@
 var largestOverlap = function (A: number[][], B: number[][]) {
-    let LA: number[] = []
-    let LB: number[] = []
-    let count: number[] = []
+    let LA: any = []
+    let LB: any = []
+    let count = {}
     let N: number = A.length
 
-    for (let i: number = 0; i < N * N; i++) {
-        if (A[i / N][i % N] === 1) {
-            LA.push(i / N * 100 + i % N)
+    for (let i: number = 0; i < N; i++) {
+        for (let j: number = 0; j < N; j++) {
+            if (A[i][j] === 1) {
+                LA.push([i, j])
+            }
+            if (B[i][j] === 1) {
+                LB.push([i, j])
+            }
         }
-        if (B[i / N][i % N] === 1) {
-            LA.push(i / N * 100 + i % N)
+
+    }
+    // dict[(a[0] - b[0]) * 100 + (a[1] - b[1])] += 1
+    // dict[(a[0] - b[0]) * magic + (a[1] - b[1])] += 1
+    for (const a of LA) {
+        for (const b of LB) {
+            let res: number = (a[0] - b[0]) * 100 + (a[1] - b[1])
+            if (!count[res]) {
+                count[(a[0] - b[0]) * 100 + (a[1] - b[1])] = 1
+            } else {
+                count[(a[0] - b[0]) * 100 + (a[1] - b[1])] += 1
+
+            }
         }
     }
 
-    for (let j: number = 0; j < LA.length; j++) {
-        for (let k: number = 0; k < LB.length; k++) {
-            count[j - k]++
-        }
+    let values: any = Object.keys(count).map(key => count[key]);
+    console.log(values, !!count)
+    if (!!count) {
+        console.log(Math.max(...values))
+        return Math.max(...values)
+    } else {
+        return 0
     }
-    console.log(count, LA, LB)
-
+    // return !!count ? Math.max(values) : 0
 
 };
 
 console.log(largestOverlap([[1, 1, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 0], [0, 1, 1], [0, 0, 1]]))
+// class Solution:
+//     def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
+//         n = len(A)
+//         magic = 100
+
+//         onesA = []
+//         onesB = []
+//         dict = collections.defaultdict(int)
+
+//         for i in range(n):
+//             for j in range(n):
+//                 if A[i][j] == 1:
+//                     onesA.append([i, j])
+//                 if B[i][j] == 1:
+//                     onesB.append([i, j])
+
+//         for a in onesA:
+//             for b in onesB:
+//                 
+
+//         return max(dict.values()) if dict else 0
