35c61ab77ad4288c65595cacbf1af3badd80e478
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-07 19:52:07 +0200

    :fire: started

--- LeetCode/Challenges/September/Week 1/AllElementsInBST.ts
+++ LeetCode/Challenges/September/Week 1/AllElementsInBST.ts
@@ -1,40 +1,39 @@
-
 // Definition for a binary tree node.
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.left = (left === undefined ? null : left)
-        this.right = (right === undefined ? null : right)
+// class TreeNode {
+//     val: number
+//     left: TreeNode | null
+//     right: TreeNode | null
+//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//         this.val = (val === undefined ? 0 : val)
+//         this.left = (left === undefined ? null : left)
+//         this.right = (right === undefined ? null : right)
+//     }
+// }
+
+function getAllElements(
+  root1: TreeNode | null,
+  root2: TreeNode | null
+): number[] {
+  let result: number[] = [];
+
+  function traverse(node: TreeNode): any {
+    if (!node) {
+      return [];
     }
+    return traverse(node.left) + ' ' + [node.val] + ' ' + traverse(node.right);
+  }
+
+  let res: any = traverse(root1) + ' ' + traverse(root2);
+  res = res.split(' ');
+
+  for (let i = 0; i < res.length; i++) {
+    console.log(res[i] !== 'NaN', res[i] === '', res[i]);
+    if (res[i] !== 'NaN') {
+      if (res[i] !== '') {
+        result.push(parseInt(res[i]));
+      }
+    }
+  }
+
+  return result.sort((a, b) => a - b);
 }
-
-
-
-function getAllElements(root1: TreeNode | null, root2: TreeNode | null): number[] {
-    let result: number[] = []
-
-    function traverse(node: TreeNode): any {
-        if (!node) {
-            return []
-        }
-        return traverse(node.left) + " " + [node.val] + " " + traverse(node.right)
-    }
-
-    let res: any = traverse(root1) + " " + traverse(root2)
-    res = res.split(' ')
-
-    for (let i = 0; i < res.length; i++) {
-        console.log(res[i] !== 'NaN', res[i] === '', res[i])
-        if (res[i] !== 'NaN') {
-            if (res[i] !== '') {
-                result.push(parseInt(res[i]))
-            }
-        }
-    }
-
-
-    return result.sort((a, b) => a - b)
-};
---
+++ LeetCode/Challenges/September/Week 2/SumOfRootToLeafBinaryNumbers.ts
@@ -0,0 +1,42 @@
+//  Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+let x = new TreeNode();
+x.val = 1;
+x.left = new TreeNode(0);
+x.right = new TreeNode(1);
+x.right.right = new TreeNode(1);
+x.right.left = new TreeNode(0);
+x.left.left = new TreeNode(0);
+x.left.right = new TreeNode(1);
+
+// console.log(x.left);
+
+function sumRootToLeaf(root: TreeNode | null): number {
+  let arr: number[] = [];
+
+  console.log(!root);
+  if (!root) return 0;
+  function recursive(tree: TreeNode) {
+    if (!tree) {
+      return;
+    }
+    console.log(root.val, root.left);
+    // return recursive(root.left);
+  }
+  let y = recursive(root);
+
+  console.log(y, 'hello');
+
+  return 0;
+}
+console.log(sumRootToLeaf(x));
