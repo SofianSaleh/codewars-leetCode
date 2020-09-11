bbb3924d361e0e597f1fe9b2b9062ca9e392a917
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-11 14:50:01 +0200

    :fire:

--- LeetCode/Challenges/September/Week 2/MaximumProductSubarray.ts
+++ LeetCode/Challenges/September/Week 2/MaximumProductSubarray.ts
@@ -1,26 +1,18 @@
 function maxProduct(nums: number[]): number {
     let product: number = nums[0]
+    let prevMin: number = nums[0]
+    let prevMax: number = nums[0]
 
     for (let i = 1; i < nums.length; i++) {
         console.log(nums[i])
-        let maximum: number = product * nums[i]
-        product = Math.max(product, maximum)
+        let minimum: number = prevMin * nums[i]
+        let maximum: number = prevMax * nums[i]
+        prevMin = Math.min(nums[i], minimum, maximum)
+        prevMax = Math.max(nums[i], minimum, maximum)
+        product = Math.max(product, prevMax)
     }
 
     return product
 };
 
 console.log(maxProduct([2, 3, -2, 4]))
-
-// ans = nums[0]
-//         prevMin = nums[0]
-//         prevMax = nums[0]
-
-//         for i in range(1, len(nums)):
-//             mini = prevMin * nums[i]
-//             maxi = prevMax * nums[i]
-//             prevMin = min(nums[i], mini, maxi)
-//             prevMax = max(nums[i], mini, maxi)
-//             ans = max(ans, prevMax)
-
-//         return ans
