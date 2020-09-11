db5b036be7b2405215fbb78e38faec1a598faca6
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-11 14:54:25 +0200

    :fire:

--- LeetCode/Challenges/September/Week 2/MaximumProductSubarray.ts
+++ LeetCode/Challenges/September/Week 2/MaximumProductSubarray.ts
@@ -18,3 +18,22 @@ console.log(maxProduct([2, 3, -2, 4]))
 console.log(maxProduct([21, -3, -2, 4]))
 console.log(maxProduct([2, -3, -2, 4]))
 console.log(maxProduct([2, -3, -2, -4]))
+
+// ######################## JAVASCRIPT ########################
+
+// var maxProduct = (nums) =>{
+//     let product = nums[0]
+//     let prevMin = nums[0]
+//     let prevMax = nums[0]
+
+//     for (let i = 1; i < nums.length; i++) {
+//         let minimum = prevMin * nums[i]
+//         let maximum = prevMax * nums[i]
+//         prevMin = Math.min(nums[i], minimum, maximum)
+//         prevMax = Math.max(nums[i], minimum, maximum)
+//         product = Math.max(product, prevMax)
+//     }
+
+//     return product
+// };
+
