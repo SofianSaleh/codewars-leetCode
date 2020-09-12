975e24541abf689046cd077c0f436aee2b651c3e
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-12 07:59:12 +0200

    :fire: trying

--- LeetCode/Challenges/September/Week 2/CombinationSumIII.ts
+++ LeetCode/Challenges/September/Week 2/CombinationSumIII.ts
@@ -1,17 +1,37 @@
+
+
+// ######################## JAVASCRIPT ########################
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function (k, n) {
+
+    return answer
+}
+console.log(combinationSum3(3, 7))
+
+
 // #########################    PYTHON  #########################
-class Solution:
-def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-def dfs(k: int, n: int, count: int, path: List[int]):
-if k == 0 and n == 0:
-answer.append(path)
-return
-if k > 0 and n == 0:
-return
-for i in range(count, 10):
-    dfs(k - 1, n - 1, i + 1, path + [i])
-answer = []
-dfs(k, n, 1, [])
-return answer
+// class Solution:
+//     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+//         def dfs(k: int, n: int, count: int, path: List[int]) -> None:
+//             if k == 0 and n == 0:
+//                 answer.append(path)
+//                 return
+//             if k == 0 or n < 0:
+//                 return
+
+//             for i in range(count, 10):
+//                 dfs(k - 1, n - i, i + 1, path + [i])
+
+//         answer = []
+
+//         dfs(k, n, 1, [])
+
+//         return answer
+
 
 
 
