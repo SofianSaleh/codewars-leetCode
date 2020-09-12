729625a43579d804c4eaf137bd01da387381f7b4
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-12 07:33:12 +0200

    :fire: trying

--- LeetCode/Challenges/September/Week 2/CombinationSumIII.ts
+++ LeetCode/Challenges/September/Week 2/CombinationSumIII.ts
@@ -3,6 +3,8 @@ class Solution:
 def combinationSum3(self, k: int, n: int) -> List[List[int]]:
 def dfs(k: int, n: int, count: int, path: List[int]):
 
+for i in range(count, 10):
+    dfs(k - 1, n - 1, i + 1, path + [i])
 answer = []
 dfs(k, n, 1, [])
 return answer
