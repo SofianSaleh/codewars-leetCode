42c1d549f9bdca9b51cd55d6d3016e2f000eb8a5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-13 12:15:55 +0200

    :fire: started

--- LeetCode/Challenges/September/Week 2/InsertIntervals.ts
+++ LeetCode/Challenges/September/Week 2/InsertIntervals.ts
@@ -3,5 +3,19 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
     let end: number = intervals.length
     let targetIndex: number | null = null
 
+    while (start < end) {
+        let middle: number = Math.floor((start + end) / 2)
 
+        if (intervals[middle][0] >= newInterval[0]) {
+            end = middle
+        } else {
+            start = middle + 1
+        }
+    }
+    targetIndex = start
+    console.log(start)
+    return [[]]
 };
+
+console.log(insert([[1, 3], [6, 9]], [2, 5]))
+console.log(insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
