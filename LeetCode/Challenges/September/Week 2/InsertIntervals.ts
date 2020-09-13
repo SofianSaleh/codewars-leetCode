8f482826607da68f6c86c24c557f3596403d5ce0
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-13 12:13:03 +0200

    :fire: started

--- LeetCode/Challenges/September/Week 2/InsertIntervals.ts
+++ LeetCode/Challenges/September/Week 2/InsertIntervals.ts
@@ -1,4 +1,7 @@
 function insert(intervals: number[][], newInterval: number[]): number[][] {
+    let start: number = 0
+    let end: number = intervals.length
+    let targetIndex: number | null = null
 
 
 };
