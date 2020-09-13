29e50f2a635b64618b73469f37ba0d81e55d8d75
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-13 21:36:26 +0200

    :fire: trying

--- LeetCode/Problems/FirstMissingInteger.js
+++ LeetCode/Problems/FirstMissingInteger.js
@@ -6,25 +6,47 @@ var firstMissingPositive = function (nums) {
     let n = nums.length
     let oneExists = false
 
-    for (let i = 0; i < n; i++) {
+    /**
+     * Change the numbers tat are 0 or down or more than the length to one
+     */
 
+    for (let i = 0; i < n; i++) {
         if (nums[i] === 1) oneExists = true
         if (nums[i] <= 0 || nums[i] >= n) {
             nums[i] = 1
         }
     }
+    console.log(nums)
+    /**
+     * If we see the number change it to negative number
+     * by using the index number - 1 let's say it gives 2 we go to the index number two and
+     * change it to negative  
+     */
+
     for (let j = 0; j < n; j++) {
         let index = Math.abs(nums[j] - 1)
 
         if (nums[index] > 0) nums[index] *= -1
     }
+    console.log(nums)
+
+    /**
+     * check if there wasn't a number one 
+     */
 
     if (!oneExists) return 1
 
+    /**
+     * Check the first positive number and return it + 1
+     */
+
     for (let k = 0; k < n; k++) {
-        if (nums[k] > 0 && oneExists) return nums[k] + 1
+        if (nums[k] > 1 && oneExists) return nums[k] + 1
     }
-    return n + 1
+    console.log(nums)
+
+    return n
 };
 
-console.log(firstMissingPositive([3, 4, -1, 16, 77]))
+console.log(firstMissingPositive([1, 2, 0]))
+// console.log(firstMissingPositive([3, 4, -1, 16, 77]))
