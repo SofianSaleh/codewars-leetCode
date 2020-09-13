24562536f97a847af790001aca57d474c05ceec8
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-13 21:27:32 +0200

    :fire: trying

--- LeetCode/Challenges/September/Week 2/InsertIntervals.ts
+++ LeetCode/Challenges/September/Week 2/InsertIntervals.ts
@@ -25,8 +25,9 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
     return ans
 };
 
-console.log(insert([[1, 3], [6, 9]], [2, 5]))
-console.log(insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
+console.log(insert([[1, 5]], [2, 3]))
+// console.log(insert([[1, 3], [6, 9]], [2, 5]))
+// console.log(insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
 
 // ######################## JAVASCRIPT ########################
 
---
+++ LeetCode/Problems/FirstMissingInteger.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+    let n = nums.length
+    let oneExists = false
+
+
+};
+
+console.log(firstMissingPositive([3, 4, -1, 16, 77]))
