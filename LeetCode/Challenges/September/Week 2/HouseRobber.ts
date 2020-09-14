30e9183bd8d5f0cf341bcd745042672f891f5239
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-14 10:58:40 +0200

    :fire: trying

--- LeetCode/Challenges/September/Week 2/HouseRobber.ts
+++ LeetCode/Challenges/September/Week 2/HouseRobber.ts
@@ -2,5 +2,9 @@ function rob(nums: number[]): number {
     let prev1: number = 0
     let prev2: number = 0
 
+    for (const num of nums) {
+
+    }
+    return prev1
 };
 console.log(rob([2, 7, 9, 3, 1])) // 12
