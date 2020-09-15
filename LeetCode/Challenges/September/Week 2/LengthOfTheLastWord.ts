1407afb16f8c289113d9e9c269f047c58ed6e7be
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-15 13:40:00 +0200

    :fire: finished

--- LeetCode/Challenges/September/Week 2/LengthOfTheLastWord.ts
+++ LeetCode/Challenges/September/Week 2/LengthOfTheLastWord.ts
@@ -44,3 +44,16 @@ console.log(lengthOfLastWord('a '))
 //         ans += 1
 //     return ans
 
+// ######################## PYTHON3 ########################
+
+// class Solution:
+//     def lengthOfLastWord(self, s: str) -> int:
+//         ans = 0
+//         i = len(s) - 1
+//     while i >= 0 and s[i] == ' '): 
+//         i -= 1
+//     while i >= 0 and s[i] !== ' ':
+//         i -= 1
+//         ans += 1
+//     return ans
+
