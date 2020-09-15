6e981d03c36a5e98dcdc702682f01e98ba17e7a5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-15 13:30:01 +0200

    :fire: started

--- LeetCode/Challenges/September/Week 2/LengthOfTheLastWord.ts
+++ LeetCode/Challenges/September/Week 2/LengthOfTheLastWord.ts
@@ -1,6 +1,15 @@
 var lengthOfLastWord = function (s) {
-    let ans = 0
     let str = s.split(' ')
+    let i = str.length - 1
+    console.log(str)
+    while (i >= 0) {
+        if (str[i] !== '') {
+            return str[i].length
+        } else {
+            i -= 1
+        }
+    }
     return str[str.length - 1].length
 
 };
+console.log(lengthOfLastWord('a '))
