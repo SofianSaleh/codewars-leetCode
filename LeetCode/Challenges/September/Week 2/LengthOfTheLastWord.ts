157a2854d273ca59bc249b0df7294a43129342d0
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-15 13:34:33 +0200

    :fire: started

--- LeetCode/Challenges/September/Week 2/LengthOfTheLastWord.ts
+++ LeetCode/Challenges/September/Week 2/LengthOfTheLastWord.ts
@@ -1,7 +1,8 @@
-var lengthOfLastWord = function (s) {
-    let str = s.split(' ')
-    let i = str.length - 1
-    console.log(str)
+
+// ######################## TYPESCRIPT ########################
+function lengthOfLastWord(s: string) {
+    let str: string[] = s.split(' ')
+    let i: number = str.length - 1
     while (i >= 0) {
         if (str[i] !== '') {
             return str[i].length
@@ -11,5 +12,21 @@ var lengthOfLastWord = function (s) {
     }
     return str[str.length - 1].length
 
-};
-console.log(lengthOfLastWord('a '))
+}
+// ######################## JAVASCRIPT ########################
+// var lengthOfLastWord = function (s) {
+//     let str = s.split(' ')
+//     let i = str.length - 1
+//     console.log(str)
+//     while (i >= 0) {
+//         if (str[i] !== '') {
+//             return str[i].length
+//         } else {
+//             i -= 1
+//         }
+//     }
+//     return str[str.length - 1].length
+
+// };
+// console.log(lengthOfLastWord('a '))
+// ######################## PYTHON ########################
