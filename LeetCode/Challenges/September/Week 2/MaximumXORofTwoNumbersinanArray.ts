744fea69dd1b446f9e54564798605c261f5fe1a5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-16 11:07:36 +0200

    :fire: understandng

--- LeetCode/Challenges/September/Week 2/MaximumXORofTwoNumbersinanArray.ts
+++ LeetCode/Challenges/September/Week 2/MaximumXORofTwoNumbersinanArray.ts
@@ -1,7 +1,32 @@
+// Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
+
+// Find the maximum result of ai XOR aj, where 0 ≤ i, j < n.
+
+// Could you do this in O(n) runtime?
+
+// Example:
+
+// Input: [3, 10, 5, 25, 2, 8]
+
+// Output: 28
+
+// Explanation: The maximum result is 5 ^ 25 = 28.
+
+// XOR:
+/**
+ * Num  Ans
+ * 0 0   0
+ * 1 0   1
+ * 0 1   1 
+ * 1 1   0
+ */
+
 
 // ######################## TYPESCRIPT ########################
 
+function findMaximumXOR(nums: number[]): number {
 
+};
 
 // ######################## JAVASCRIPT ########################
 
