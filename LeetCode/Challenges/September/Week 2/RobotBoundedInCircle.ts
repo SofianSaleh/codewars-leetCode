f818f833713e7a32f6b942f5f1e6853f006046ea
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-17 11:20:16 +0200

    :fire: finished

--- LeetCode/Challenges/September/Week 2/RobotBoundedInCircle.ts
+++ LeetCode/Challenges/September/Week 2/RobotBoundedInCircle.ts
@@ -30,8 +30,26 @@ console.log(isRobotBounded("GGLLGGL"))
  * @param {string} instructions
  * @return {boolean}
  */
-// var isRobotBounded = function(instructions) {
+// var isRobotBounded = function (instructions) {
+//     let x = 0
+//     let y = 0
+//     let d = 0
+//     //                              up    down 
+//     let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
+//     for (const instruction of instructions) {
+//         if (instruction === 'G') {
+//             x += directions[d][0]
+//             y += directions[d][1]
+//         } else if (instruction === 'L') {
+//             d = (d + 3) % 4
+
+//         } else {
+//             d = (d + 1) % 4
+//         }
+
+//     }
+//     return (x === 0 && y === 0) || d > 0 ? true : false
 // };
 
 // ######################## PYTHON ########################
@@ -42,9 +60,40 @@ console.log(isRobotBounded("GGLLGGL"))
 //         :type instructions: str
 //         :rtype: bool
 //         """
+        // x = 0
+        // y = 0
+        // d = 0                           
+        // directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        // for instruction in instructions:
+        //     if instruction == 'G':
+        //         x += directions[d][0]
+        //         y += directions[d][1]
+        //     elif instruction == 'L':
+        //         d = (d + 3) % 4
+
+        //     else:
+        //         d = (d + 1) % 4
+        // return (x,y)== (0, 0) or d > 0
+
 
 
 // ######################## PYTHON3 ########################
 
 // class Solution:
 //     def isRobotBounded(self, instructions: str) -> bool:
+        // x = 0
+        // y = 0
+        // d = 0                           
+        // directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        // for instruction in instructions:
+        //     if instruction == 'G':
+        //         x += directions[d][0]
+        //         y += directions[d][1]
+        //     elif instruction == 'L':
+        //         d = (d + 3) % 4
+
+        //     else:
+        //         d = (d + 1) % 4
+        // return (x,y)== (0, 0) or d > 0
