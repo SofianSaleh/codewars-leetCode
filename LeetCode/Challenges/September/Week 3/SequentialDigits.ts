c1816eb6c656ecf001fab480bd8b1e109744bee3
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-19 14:46:52 +0200

    :fire: started

--- LeetCode/Challenges/September/Week 3/SequentialDigits.ts
+++ LeetCode/Challenges/September/Week 3/SequentialDigits.ts
@@ -17,24 +17,70 @@
 
 // ######################## TYPESCRIPT ########################
 
+console.time('exe time');
 function sequentialDigits(low: number, high: number): number[] {
   let result: number[] = [];
-  let nums: string = '123456789';
 
-  for (let i: number = 2; i < nums.length; i++) {
-    for (let j: number = 0; j < nums.length; j++) {
-      let num: number = parseInt(nums.substring(j, j + i));
-      console.log(nums);
+  for (let i = 1; i <= 8; i++) {
+    let next: number = 0;
 
-      if (num > high) return result;
-      if (num >= low) result.push(num);
+    for (let j = i; j < 10; j++) {
+      next = next * 10 + j;
+      if (next >= low && next < high) {
+        result.push(next);
+      } else if (next > high) break;
     }
   }
+  result = result.sort((a, b) => a - b);
   return result;
 }
+console.timeEnd('exe time');
 
-console.log(sequentialDigits(100, 300));
+// console.log(sequentialDigits(100, 300));
 // console.log(sequentialDigits(1000, 13000));
+let x = sequentialDigits(10, 1000000000);
+console.log(
+  x
+  //   ===
+  //     [
+  //       12,
+  //       23,
+  //       34,
+  //       45,
+  //       56,
+  //       67,
+  //       78,
+  //       89,
+  //       123,
+  //       234,
+  //       345,
+  //       456,
+  //       567,
+  //       678,
+  //       789,
+  //       1234,
+  //       2345,
+  //       3456,
+  //       4567,
+  //       5678,
+  //       6789,
+  //       12345,
+  //       23456,
+  //       34567,
+  //       45678,
+  //       56789,
+  //       123456,
+  //       234567,
+  //       345678,
+  //       456789,
+  //       1234567,
+  //       2345678,
+  //       3456789,
+  //       12345678,
+  //       23456789,
+  //       123456789,
+  // ]
+);
 
 // ######################## JAVASCRIPT ########################
 
