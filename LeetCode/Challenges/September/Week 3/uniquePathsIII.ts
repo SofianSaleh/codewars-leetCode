cddfd5e8816fb70dabd338a6d9f6e28f744c07af
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-20 15:38:03 +0200

    :fire:

--- LeetCode/Challenges/September/Week 3/uniquePathsIII.ts
+++ LeetCode/Challenges/September/Week 3/uniquePathsIII.ts
@@ -3,14 +3,14 @@ let ey: number;
 
 function uniquePathsIII(grid: number[][]): number {
   let rows: number = grid.length;
-  let columns: number = grid[0].length;
+  let cols: number = grid[0].length;
 
   let non_obstacles: any = 0;
   let start_row: number = 0;
   let start_col: number = 0;
 
   for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
+    for (let j = 0; j < cols; j++) {
       let cell = grid[i][j];
       if (cell >= 0) non_obstacles += 1;
       if (cell == 1) {
@@ -22,40 +22,37 @@ function uniquePathsIII(grid: number[][]): number {
 
   let path_count = 0;
 
-        function backtrack(row, col, remain){
-            
-        }
-            # we need to modify this external variable
-            nonlocal path_count
+  function backtrack(row: number, col: number, remain: number) {
+    if (grid[row][col] == 2 && remain == 1) {
+      path_count += 1;
+      return;
+    }
+    let temp = grid[row][col];
+    grid[row][col] = -4;
+    remain -= 1;
+    let x = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
 
-            # base case for the termination of backtracking
-            if grid[row][col] == 2 and remain == 1:
-                # reach the destination
-                path_count += 1
-                return
+    for (let i = 0; i < x.length; i++) {
+      let next_row = row + x[i][0];
+      let next_col = col + x[i][1];
 
-            # mark the square as visited. case: 0, 1, 2 
-            temp = grid[row][col] 
-            grid[row][col] = -4
-            remain -= 1   # we now have one less square to visit
+      if (
+        !(next_row >= 0 && next_row < rows && next_col >= 0 && next_col < cols)
+      )
+        continue;
+      if (grid[next_row][next_col] < 0) continue;
 
-            # explore the 4 potential directions around
-            for ro, co in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                next_row, next_col = row + ro, col + co
+      backtrack(next_row, next_col, remain);
+    }
 
-                if not (0 <= next_row < rows and 0 <= next_col < cols):
-                    # invalid coordinate
-                    continue
-                if grid[next_row][next_col] < 0:
-                    # either obstacle or visited square
-                    continue
+    grid[row][col] = temp;
+  }
+  backtrack(start_row, start_col, non_obstacles);
 
-                backtrack(next_row, next_col, remain)
-
-            # unmark the square after the visit
-            grid[row][col] = temp
-
-        backtrack(start_row, start_col, non_obstacles)
-
-        return path_count
+  return path_count;
 }
