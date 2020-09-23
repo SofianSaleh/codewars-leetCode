bb118c982c89853ce77eb9a423f95d02b0acef26
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-23 20:23:39 +0200

    :fire: start

--- LeetCode/Challenges/September/Week 4/GasStation.ts
+++ LeetCode/Challenges/September/Week 4/GasStation.ts
@@ -2,4 +2,9 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
   let fuel: number = 0;
   let net: number = 0;
   let sum: number = 0;
+
+  for (let i = 0; i < gas.length; i++) {
+    net += gas[i] - cost[i];
+    sum += gas[i] - cost[i];
+  }
 }
