c117f39d7f56ea674058bc8666eb327c19bd773b
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-24 12:06:27 +0200

    :fire:

--- LeetCode/Challenges/September/Week 4/FindTheDiffrence.ts
+++ LeetCode/Challenges/September/Week 4/FindTheDiffrence.ts
@@ -2,7 +2,8 @@ function findTheDifference(s: string, t: string): string {
   let arr = t.split('');
   for (let i = 0; i < t.length; i++) {
     let index = arr.indexOf(s[i]);
-    arr.splice(index, 1);
+    if (index !== -1) arr.splice(index, 1);
   }
   return arr.join('');
 }
+console.log(findTheDifference('abcd', 'abcde'));
