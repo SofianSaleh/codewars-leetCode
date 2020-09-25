dd463e70a338729b4375e756e284b8666bfa80d3
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-25 19:29:26 +0200

    :fire:

--- LeetCode/Challenges/September/Week 4/LargestNumber.ts
+++ LeetCode/Challenges/September/Week 4/LargestNumber.ts
@@ -27,19 +27,13 @@ console.log(largestNumber([3, 30, 34, 5, 9]));
 console.log(largestNumber([10, 2]));
 
 // ##################### JAVASCRIPT #####################
-
-function largestNumber(nums: number[]): string {
-  var res = nums
-    .sort(function (a, b) {
-      var str1 = '' + a + b;
-      var str2 = '' + b + a;
-      if (str1 === str2) return 0;
-      return str1 > str2 ? -1 : 1;
-    })
-    .join('');
-  return res[0] === '0' ? '0' : res;
-}
-
-console.log(largestNumber([1, 2, 3, 3]));
-console.log(largestNumber([3, 30, 34, 5, 9]));
-console.log(largestNumber([10, 2]));
+var largestNumber = (nums: number[]) => {};
+var res = nums
+  .sort(function (a, b) {
+    var str1 = '' + a + b;
+    var str2 = '' + b + a;
+    if (str1 === str2) return 0;
+    return str1 > str2 ? -1 : 1;
+  })
+  .join('');
+return res[0] === '0' ? '0' : res;
