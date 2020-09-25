904ad580c6759c12897eae4b7ec3d3c44767d546
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-25 19:30:16 +0200

    :fire:

--- LeetCode/Challenges/September/Week 4/LargestNumber.ts
+++ LeetCode/Challenges/September/Week 4/LargestNumber.ts
@@ -22,19 +22,19 @@ function largestNumber(nums: number[]): string {
   return res[0] === '0' ? '0' : res;
 }
 
-console.log(largestNumber([1, 2, 3, 3]));
-console.log(largestNumber([3, 30, 34, 5, 9]));
-console.log(largestNumber([10, 2]));
+// console.log(largestNumber([1, 2, 3, 3]));
+// console.log(largestNumber([3, 30, 34, 5, 9]));
+// console.log(largestNumber([10, 2]));
 
 // ##################### JAVASCRIPT #####################
-var largestNumber = (nums: number[]) => {
-  var res = nums
-    .sort(function (a, b) {
-      var str1 = '' + a + b;
-      var str2 = '' + b + a;
-      if (str1 === str2) return 0;
-      return str1 > str2 ? -1 : 1;
-    })
-    .join('');
-  return res[0] === '0' ? '0' : res;
-};
+// var largestNumber = (nums: number[]) => {
+//   var res = nums
+//     .sort(function (a, b) {
+//       var str1 = '' + a + b;
+//       var str2 = '' + b + a;
+//       if (str1 === str2) return 0;
+//       return str1 > str2 ? -1 : 1;
+//     })
+//     .join('');
+//   return res[0] === '0' ? '0' : res;
+// };
