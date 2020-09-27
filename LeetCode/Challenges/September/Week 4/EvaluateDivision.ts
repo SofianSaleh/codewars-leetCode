5a876bff508f5a0d655a83c4b4eb21345c65c557
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-27 11:31:12 +0200

    :fire:

--- LeetCode/Challenges/September/Week 4/EvaluateDivision.ts
+++ LeetCode/Challenges/September/Week 4/EvaluateDivision.ts
@@ -49,20 +49,20 @@ function calcEquation(
     else graph[y] = { [x]: 1 / v };
   }
   function dfs(s: string, t: string): number {
-    console.log(s, t);
+    // console.log(s, t);
     if (!graph[s]) return -1;
     // console.log(t === s);
     if (t === s) return 1;
 
     // console.log(Object.keys(graph[s]));
     for (const node of Object.keys(graph[s])) {
-      console.log(node, t);
+      console.log(node === t, graph[s][node]);
 
       if (node === t) return graph[s][node];
       else if (!visited[node]) {
         visited[node] = node;
         let v = dfs(node, t);
-        console.log(node, t);
+        // console.log(node, t);
         if (v !== -1) return graph[s][node] * v;
       }
       return -1;
