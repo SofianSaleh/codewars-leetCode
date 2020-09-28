04be56dc11daa6b31b1f7f638ec3dd928400a044
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-28 12:55:09 +0200

    :fire:

---
+++ LeetCode/Challenges/September/Week 4/SubarrayProductLessThanK.ts
@@ -0,0 +1,28 @@
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+  if (k <= 1) return 0;
+
+  let ans: number = 0;
+  let prod: number = 1;
+
+  let j: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    prod *= nums[i];
+    while (prod >= k) {
+      prod /= nums[j];
+      j += 1;
+    }
+    ans += i - j + 1;
+  }
+
+  return ans;
+}
+
+// for i, num in enumerate(nums):
+// prod *= num
+// while prod >= k:
+//     prod /= nums[j]
+//     j += 1
+// ans += i - j + 1
+
+// return ans
