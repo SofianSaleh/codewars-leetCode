2414f10d6a8c5d139dcb93167f2ba2f0acfcae45
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-30 11:16:28 +0200

    :fire:

--- LeetCode/Challenges/September/Week 5/FirstMissingPositive.ts
+++ LeetCode/Challenges/September/Week 5/FirstMissingPositive.ts
@@ -15,11 +15,11 @@
 // Follow up:
 
 // Your algorithm should run in O(n) time and uses constant extra space.
-
+// ? [0,4,3,1,7]
 function firstMissing(nums: number[]): number {
-  if (nums.length === 0) return 1;
-  let n: number = nums.length;
-  let oneExists: boolean = false;
+  if (nums.length === 0) return 1; //? skip
+  let n: number = nums.length; //? 5
+  let oneExists: boolean = false; //? false
 
   //   ? Change all the negative or the numbers who are length bigger than the array length to 1
   for (let i = 0; i < n; i++) {
@@ -28,16 +28,18 @@ function firstMissing(nums: number[]): number {
       nums[i] = 1;
     }
   }
-  if (!oneExists) return 1;
+  //? [1,4,3,1,1] oneExists = true
+  if (!oneExists) return 1; //? !true
   // ? Any number that we are going through we change the index to  -1 so we know that we saw it
   for (let j = 0; j < n; j++) {
     let index = Math.abs(nums[j]) - 1;
 
     if (nums[index] > 0) nums[index] *= -1;
   }
-
+  //? [1,4,-1,-1,1]
+  // ? Now we return the number where the index is bigger than 0 cause all of the nmbers are -1
   for (let k = 0; k < n; k++) {
-    if (nums[k] > 0) return k + 1;
+    if (nums[k] > 0) return k + 1; //? return 4
   }
 
   return n + 1;
