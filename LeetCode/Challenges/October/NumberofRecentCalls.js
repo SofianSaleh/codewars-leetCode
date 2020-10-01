b2977ec7892d540b3dd4522779a5373ac3322934
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-01 13:53:34 +0200

    :fire:

--- LeetCode/Challenges/October/NumberofRecentCalls.js
+++ LeetCode/Challenges/October/NumberofRecentCalls.js
@@ -3,5 +3,13 @@ class RecentCounter {
     this.queue = [];
   }
 
-  ping(t) {}
+  ping(t) {
+    this.queue.push(t);
+    while (this.queue[0] < t - 3000) {
+      this.queue.shift();
+    }
+
+    //return the size of the queue
+    return this.queue.length;
+  }
 }
