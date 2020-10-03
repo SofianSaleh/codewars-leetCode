bcc050b4345d1a3a6bf18548134905bce9c43d22
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-03 10:05:21 +0200

    :fire:

--- LeetCode/Challenges/October/Week 1/K-diffPairsinanArray.js
+++ LeetCode/Challenges/October/Week 1/K-diffPairsinanArray.js
@@ -7,17 +7,20 @@ var findPairs = function (nums, k) {
   if (k < 0) return 0;
 
   let ans = 0;
-    let dict = {}
-  for(const [index, element] of nums.entries()){
-      if(!!dict[index]){
-          dict[index] = element
-      }
+  let dict = {};
+  for (const [index, element] of nums.entries()) {
+    if (!!dict[index]) {
+      dict[index] = element;
+    }
   }
-  for(const [index, element] of nums.entries()){
-      let target = num + k
-      if(!!dict[target]+)
+  for (const [index, element] of nums.entries()) {
+    let target = num + k;
+    if (!!dict[target] && dict[target] != index) {
+      ans += 1;
+      delete dict[target];
+    }
   }
-  
+  return ans;
 };
 
 // def findPairs(self, nums: List[int], k: int) -> int:
