e38039a200eef7c56e7d12703eca442141ab6d75
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-12 12:27:16 +0200

    :fire:

--- LeetCode/Challenges/October/Week 2/BuddyStrings.js
+++ LeetCode/Challenges/October/Week 2/BuddyStrings.js
@@ -62,3 +62,16 @@ var buddyStrings = function (A, B) {
 console.log(buddyStrings('ab', 'ba'));
 console.log(buddyStrings('aa', 'aa'));
 console.log(buddyStrings('aaaaaaabc', 'aaaaaaacb'));
+
+//  ################## PYTHON  ################### //
+
+// class Solution:
+//     def buddyStrings(self, A: str, B: str) -> bool:
+//         if len(A) != len(B):
+//             return False
+//         if A == B and len(set(A)) < len(A):
+//             return True
+
+//         diff = [(a, b) for a, b in zip(A, B) if a != b]
+
+//         return len(diff) == 2 and diff[0] == diff[1][::-1]
