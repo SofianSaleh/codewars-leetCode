62050bc6b461ab92c43603085b817f41ad179fcf
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-13 19:08:39 +0200

    :fire:

--- LeetCode/Challenges/October/Week 2/SortList.js
+++ LeetCode/Challenges/October/Week 2/SortList.js
@@ -76,16 +76,17 @@ var sortList = function (head) {
     return dummy.next, tail;
   };
 
-  length = 0
-  curr = head
-  while curr:
-      length += 1
-      curr = curr.next
+  let length = 0;
+  let curr = head;
+  while (curr) {
+    length += 1;
+    curr = curr.next;
+  }
 
-  dummy = ListNode(0)
-  dummy.next = head
+  let dummy = ListNode(0);
+  dummy.next = head;
 
-  k = 1
+  k = 1;
 };
 
 console.log(sortList(x));
