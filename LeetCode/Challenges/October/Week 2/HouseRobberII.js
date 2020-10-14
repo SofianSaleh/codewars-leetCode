a02338e91524ee0fd0db7ce41be353fc5e6ab685
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-14 15:52:25 +0200

    :fire:

--- LeetCode/Challenges/October/Week 2/HouseRobberII.js
+++ LeetCode/Challenges/October/Week 2/HouseRobberII.js
@@ -27,4 +27,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function (nums) {};
+var rob = function (nums) {
+  var rob1 = function (l, r) {
+    let dp1 = 0;
+    let dp2 = 0;
+    //             for i in range(l, r + 1):
+    //                 temp = dp1
+    //                 dp1 = max(dp1, dp2 + nums[i])
+    //                 dp2 = temp
+    //             return dp1
+  };
+};
+
+// class Solution:
+//     def rob(self, nums: List[int]) -> int:
+//         def rob(l: int, r: int) -> int:
+
+//         if not nums:
+//             return 0
+//         if len(nums) < 2:
+//             return nums[0]
+
+//         return max(rob(0, len(nums) - 2), rob(1, len(nums) - 1))
