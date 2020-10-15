094a167c12e5f8f77caab52a8202bd6070dba066
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-15 15:27:47 +0200

    :fire:

--- LeetCode/Challenges/October/Week 3/RotateArray.js
+++ LeetCode/Challenges/October/Week 3/RotateArray.js
@@ -40,7 +40,7 @@ var rotate = function (nums, k) {
 };
 
 var reverse = function (nums, l, r) {
-  //         while l < r:
+          while l < r:
   //             nums[l], nums[r] = nums[r], nums[l]
   //             l += 1
   //             r -= 1
