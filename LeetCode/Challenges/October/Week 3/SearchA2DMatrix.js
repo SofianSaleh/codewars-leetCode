0fae83deaedfc0842e3d037ddd91cda64a7b19f5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-16 14:55:54 +0200

    :fire:

--- LeetCode/Challenges/October/Week 3/SearchA2DMatrix.js
+++ LeetCode/Challenges/October/Week 3/SearchA2DMatrix.js
@@ -97,24 +97,24 @@ console.log(
   )
 ); // false
 console.log(searchMatrix1([], 0)); // false
-// console.log(
-//   searchMatrix(
-//     [
-//       [1, 3, 5, 7],
-//       [10, 11, 16, 20],
-//       [23, 30, 34, 50],
-//     ],
-//     3
-//   )
-// ); // True
-// console.log(
-//   searchMatrix(
-//     [
-//       [1, 3, 5, 7],
-//       [10, 11, 16, 20],
-//       [23, 30, 34, 50],
-//     ],
-//     13
-//   )
-// ); // false
-// console.log(searchMatrix([], 0)); // false
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 50],
+    ],
+    3
+  )
+); // True
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 50],
+    ],
+    13
+  )
+); // false
+console.log(searchMatrix([], 0)); // false
