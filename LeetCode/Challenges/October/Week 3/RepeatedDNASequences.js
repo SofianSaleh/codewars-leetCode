530b0dc5519e5f3263c2a39777ae3346b6793e8d
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-17 14:01:44 +0200

    :fire:

--- LeetCode/Challenges/October/Week 3/RepeatedDNASequences.js
+++ LeetCode/Challenges/October/Week 3/RepeatedDNASequences.js
@@ -2,8 +2,6 @@
 
 // Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
 
- 
-
 // Example 1:
 
 // Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
@@ -12,9 +10,14 @@
 
 // Input: s = "AAAAAAAAAAAAA"
 // Output: ["AAAAAAAAAA"]
- 
 
 // Constraints:
 
 // 0 <= s.length <= 105
 // s[i] is 'A', 'C', 'G', or 'T'
+
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function (s) {};
