1e4b142bc43174bfc5a658872c53d3cc8f3a6bd1
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-20 11:27:58 +0200

    :fire:

--- LeetCode/Challenges/October/Week 3/CloneGraph.js
+++ LeetCode/Challenges/October/Week 3/CloneGraph.js
@@ -88,8 +88,8 @@ class Solution:
 
         self.lookup[node] = Node(node.val, [])
         for neighbor in node.neighbors:
-//             self.lookup[node].neighbors.append(self.cloneGraph(neighbor))
+            self.lookup[node].neighbors.append(self.cloneGraph(neighbor))
 
-//         return self.lookup[node]
+        return self.lookup[node]
 
 //     lookup = {}
