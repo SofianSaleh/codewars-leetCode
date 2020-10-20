9b80b4c858cec7d7ead0b0098ed6f07bf535a246
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-20 11:28:07 +0200

    :fire:

--- LeetCode/Challenges/October/Week 3/CloneGraph.js
+++ LeetCode/Challenges/October/Week 3/CloneGraph.js
@@ -74,22 +74,19 @@
 //   return lookup[node];
 // };
 
-
 //  ######################## PYTHON ##########################
 
+// class Solution:
+//     def cloneGraph(self, node: 'Node') -> 'Node':
+//         if not node:
+//             return None
+//         if node in self.lookup:
+//             return self.lookup[node]
 
+//         self.lookup[node] = Node(node.val, [])
+//         for neighbor in node.neighbors:
+//             self.lookup[node].neighbors.append(self.cloneGraph(neighbor))
 
-class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        if not node:
-            return None
-        if node in self.lookup:
-            return self.lookup[node]
+//         return self.lookup[node]
 
-        self.lookup[node] = Node(node.val, [])
-        for neighbor in node.neighbors:
-            self.lookup[node].neighbors.append(self.cloneGraph(neighbor))
-
-        return self.lookup[node]
-
-    lookup = {}
+//     lookup = {}
