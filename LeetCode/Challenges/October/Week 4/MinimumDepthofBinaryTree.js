d80a210a2bb155b312e1688b30d5b3f238fe5ee1
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-22 11:35:46 +0200

    :fire:

--- LeetCode/Challenges/October/Week 4/MinimumDepthofBinaryTree.js
+++ LeetCode/Challenges/October/Week 4/MinimumDepthofBinaryTree.js
@@ -18,6 +18,20 @@
 // The number of nodes in the tree is in the range [0, 105].
 // -1000 <= Node.val <= 1000
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {};
+
 // ###################### PYTHON ####################### //
 // # Definition for a binary tree node.
 // # class TreeNode:
