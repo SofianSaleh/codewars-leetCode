33a7be84ccd6b080306baeadbd93de775306a0b5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-26 19:26:02 +0200

    :fire:

--- LeetCode/Challenges/October/Week 4/ChampagneTower.js
+++ LeetCode/Challenges/October/Week 4/ChampagneTower.js
@@ -44,14 +44,14 @@ var champagneTower = function (poured, query_row, query_glass) {
     }
   }
   dp[0][0] = poured;
-  for (let i = 0; i < query_row; i++) {
-    for (let j = 0; j < i + 1; j++) {
-      if (dp[i][j] > 1) {
-        dp[i + 1][j] += (dp[i][j] - 1) / 2.0;
-        dp[i + 1][j + 1] += (dp[i][j] - 1) / 2.0;
-      }
-    }
-  }
+  //   for (let i = 0; i < query_row; i++) {
+  //     for (let j = 0; j < i + 1; j++) {
+  //       if (dp[i][j] > 1) {
+  //         dp[i + 1][j] += (dp[i][j] - 1) / 2.0;
+  //         dp[i + 1][j + 1] += (dp[i][j] - 1) / 2.0;
+  //       }
+  //     }
+  //   }
   return Math.min(1, dp[query_row][query_glass]);
 };
 
