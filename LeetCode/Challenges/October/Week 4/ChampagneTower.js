b26d4a699bd41c91c5485421f2987957db83b3c1
SofianSaleh <sofiane.saleh11@gmail.com>
2020-10-26 19:25:41 +0200

    :fire:

--- LeetCode/Challenges/October/Week 4/ChampagneTower.js
+++ LeetCode/Challenges/October/Week 4/ChampagneTower.js
@@ -57,4 +57,4 @@ var champagneTower = function (poured, query_row, query_glass) {
 
 console.log(champagneTower(1, 1, 1));
 console.log(champagneTower(2, 1, 1));
-console.log(champagneTower(100000009, 33, 17));
+// console.log(champagneTower(100000009, 33, 17));
