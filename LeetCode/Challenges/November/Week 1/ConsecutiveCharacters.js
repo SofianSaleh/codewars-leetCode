5cca736783070bdbe6c2349b33f014190615b32b
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-03 11:22:19 +0200

    :fire:

--- LeetCode/Challenges/November/Week 1/ConsecutiveCharacters.js
+++ LeetCode/Challenges/November/Week 1/ConsecutiveCharacters.js
@@ -46,5 +46,5 @@ var maxPower = function (s) {
 };
 console.log(maxPower('leetcode'));
 console.log(maxPower('abbcccddddeeeeedcba'));
-console.log(maxPower('abbcccddddeeeeedcba'));
-console.log(maxPower('abbcccddddeeeeedcba'));
+console.log(maxPower('triplepillooooow'));
+console.log(maxPower('hooraaaaaaaaaaay'));
