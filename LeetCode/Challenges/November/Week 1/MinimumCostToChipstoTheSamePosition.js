69406666d724b78c2e3b3b36b1532883427e3b8a
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-05 11:03:49 +0200

    :fire:

--- LeetCode/Challenges/November/Week 1/MinimumCostToChipstoTheSamePosition.js
+++ LeetCode/Challenges/November/Week 1/MinimumCostToChipstoTheSamePosition.js
@@ -27,3 +27,12 @@
 
 // 1 <= position.length <= 100
 // 1 <= position[i] <= 10^9
+
+// class Solution:
+//     def minCostToMoveChips(self, chips: List[int]) -> int:
+//         count = [0] * 2
+
+//         for chip in chips:
+//             count[chip % 2] += 1
+
+//         return min(count[0], count[1])
