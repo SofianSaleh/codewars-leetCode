bc3453e7815d054ed55e6e2f4d938996cd2d9eb0
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-07 18:42:55 +0200

    :fire:

--- LeetCode/Challenges/November/Week 1/AddTwoNumbersII.js
+++ LeetCode/Challenges/November/Week 1/AddTwoNumbersII.js
@@ -17,6 +17,14 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
