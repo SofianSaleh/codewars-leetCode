4535171393d1973b59a38c8f64f5874429f6d425
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-08 11:01:01 +0200

    :fire:

--- LeetCode/Challenges/November/Week 2/BinaryTreeTilt.js
+++ LeetCode/Challenges/November/Week 2/BinaryTreeTilt.js
@@ -54,4 +54,29 @@ class TreeNode {
  * @param {TreeNode} root
  * @return {number}
  */
-var findTilt = function (root) {};
+var findTilt = function (root) {
+  let ans = 0;
+
+  Sum();
+  return ans;
+};
+
+// lass Solution {
+//   public int findTilt(TreeNode root) {
+//     sum(root);
+//     return ans;
+//   }
+
+//   private int ans = 0;
+
+//   private int sum(TreeNode root) {
+//     if (root == null)
+//       return 0;
+
+//     final int l = sum(root.left);
+//     final int r = sum(root.right);
+//     ans += Math.abs(l - r);
+
+//     return root.val + l + r;
+//   }
+// }
