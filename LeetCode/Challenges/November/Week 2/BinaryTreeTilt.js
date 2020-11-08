a0bd1ade4d6dd8592a5aa80509e1acd1bc0f8d13
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-08 11:09:47 +0200

    :fire:

--- LeetCode/Challenges/November/Week 2/BinaryTreeTilt.js
+++ LeetCode/Challenges/November/Week 2/BinaryTreeTilt.js
@@ -42,13 +42,13 @@
  * }
  */
 
-class TreeNode {
-  constructor(val, left, right) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+// class TreeNode {
+//   constructor(val, left, right) {
+//     this.val = val === undefined ? 0 : val;
+//     this.left = left === undefined ? null : left;
+//     this.right = right === undefined ? null : right;
+//   }
+// }
 
 /**
  * @param {TreeNode} root
