135f52c3599d48d7ae07da22af5ab70c68ef33ff
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-10 10:24:39 +0200

    :fire:

--- LeetCode/Challenges/November/Week 2/FlippinganImage.js
+++ LeetCode/Challenges/November/Week 2/FlippinganImage.js
@@ -20,3 +20,11 @@
 
 // 1 <= A.length = A[0].length <= 20
 // 0 <= A[i][j] <= 1
+
+// class Solution:
+//     def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+//         for row in A:
+//             for i in range((len(row) + 1) // 2):
+//                 row[i], row[~i] = row[~i] ^ 1, row[i] ^ 1
+
+//         return A
