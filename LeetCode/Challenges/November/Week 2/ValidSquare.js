cb077be27f5d2045f1415066a52ac8714d4f2623
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-11 15:16:41 +0200

    :fire:

--- LeetCode/Challenges/November/Week 2/ValidSquare.js
+++ LeetCode/Challenges/November/Week 2/ValidSquare.js
@@ -21,7 +21,7 @@
  * @return {boolean}
  */
 var validSquare = function (p1, p2, p3, p4) {
-  if (p1[0] === p1[1] && p3[0] === p3[1] && p2[0] === p4[1] && p2[1] === p4[0])
+  if (p1[0] === p1[1] && p2[0] === p2[1] && p3[0] === p4[1] && p3[1] === p4[0])
     return true;
   else return false;
 };
