db2d4fffb0e6a60960931530ff404b5c7ee5af72
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-11 15:21:04 +0200

    :fire:

--- LeetCode/Challenges/November/Week 2/ValidSquare.js
+++ LeetCode/Challenges/November/Week 2/ValidSquare.js
@@ -21,9 +21,20 @@
  * @return {boolean}
  */
 var validSquare = function (p1, p2, p3, p4) {
-  if (p1[0] === p1[1] && p2[0] === p2[1] && p3[0] === p4[1] && p3[1] === p4[0])
-    return true;
-  else return false;
+  var dist = function (p1, p2) {
+    return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2;
+  };
 };
 
 console.log(validSquare([0, 0], [1, 1], [1, 0], [0, 1]));
+// def dist(p1: List[int], p2: List[int]) -> int:
+//             return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+
+//         distSet = set()
+//         points = [p1, p2, p3, p4]
+
+//         for i in range(4):
+//             for j in range(i + 1, 4):
+//                 distSet.add(dist(points[i], points[j]))
+
+//         return 0 not in distSet and len(distSet) == 2
