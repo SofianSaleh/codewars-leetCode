2ff32d3417c360085a6feaf01c6702b4d7d487e5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-13 11:25:54 +0200

    :fire:

--- LeetCode/Challenges/November/Week 2/PopulatingNextRightPointersinEachNode.js
+++ LeetCode/Challenges/November/Week 2/PopulatingNextRightPointersinEachNode.js
@@ -36,11 +36,13 @@
  * };
  */
 
-function Node(val, left, right, next) {
-  this.val = val === undefined ? null : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-  this.next = next === undefined ? null : next;
+class Node {
+  constructor(val, left, right, next) {
+    this.val = val === undefined ? null : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+    this.next = next === undefined ? null : next;
+  }
 }
 
 /**
