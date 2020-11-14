c2a80fc8fd04d201a9acb37624bb496115180589
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-14 15:25:01 +0200

    :fire:

--- LeetCode/Challenges/November/Week 2/PoorPigs.js
+++ LeetCode/Challenges/November/Week 2/PoorPigs.js
@@ -26,4 +26,3 @@ var poorPigs = function (buckets, minutesToDie, minutesToTest) {
 
 // class Solution:
 //     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-//         return math.ceil(math.log(buckets) / math.log(minutesToTest // minutesToDie + 1))
