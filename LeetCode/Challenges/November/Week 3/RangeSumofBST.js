1290e66fbbe813b7a914f3912c46674602bf0b9c
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-15 13:27:23 +0200

    :fire:

--- LeetCode/Challenges/November/Week 3/RangeSumofBST.js
+++ LeetCode/Challenges/November/Week 3/RangeSumofBST.js
@@ -17,6 +17,21 @@
 // 1 <= low <= high <= 105
 // All Node.val are unique.
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+
 var rangeSumBST = function (root, low, high) {};
 
 // class Solution {
