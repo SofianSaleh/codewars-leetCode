c178df22f1a444116f81fc863d3889982e540076
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-15 13:27:40 +0200

    :fire:

--- LeetCode/Challenges/November/Week 3/RangeSumofBST.js
+++ LeetCode/Challenges/November/Week 3/RangeSumofBST.js
@@ -32,10 +32,12 @@
  * @return {number}
  */
 
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
 }
 
 var rangeSumBST = function (root, low, high) {};
