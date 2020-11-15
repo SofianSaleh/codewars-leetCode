27f12bb39432eb4483cf5813cd46440a527aaf98
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-15 13:30:27 +0200

    :fire:

--- LeetCode/Challenges/November/Week 3/RangeSumofBST.js
+++ LeetCode/Challenges/November/Week 3/RangeSumofBST.js
@@ -50,15 +50,3 @@ var rangeSumBST = function (root, low, high) {
     rangeSumBST(root.right, low, high)
   );
 };
-
-// class Solution {
-//     public int rangeSumBST(TreeNode root, int L, int R) {
-//       if (root == null)
-//         return 0;
-//       if (root.val < L)
-//         return rangeSumBST(root.right, L, R);
-//       if (root.val > R)
-//         return rangeSumBST(root.left, L, R);
-//       return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
-//     }
-//   }
