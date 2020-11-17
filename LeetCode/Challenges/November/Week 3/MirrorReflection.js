c136fced2e27da2a075dbf48d5e12ac2bcda11e5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-17 13:02:16 +0200

    :fire:

--- LeetCode/Challenges/November/Week 3/MirrorReflection.js
+++ LeetCode/Challenges/November/Week 3/MirrorReflection.js
@@ -27,13 +27,14 @@ var mirrorReflection = function (p, q) {
   let n = 1;
 
   while (m * p !== n * q) {
+    console.log(m * p, n * q);
     n += 1;
     m = Math.ceil((n * q) / p);
     console.log(m, (n * q) / p);
-    if (m % 2 == 0 && n % 2 == 1) return 0;
-    if (m % 2 == 1 && n % 2 == 1) return 1;
-    if (m % 2 == 1 && n % 2 == 0) return 2;
   }
+  if (m % 2 == 0 && n % 2 == 1) return 0;
+  if (m % 2 == 1 && n % 2 == 1) return 1;
+  if (m % 2 == 1 && n % 2 == 0) return 2;
 };
 
 // console.log(mirrorReflection(2, 1));
