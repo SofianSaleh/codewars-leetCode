fd84887ac3e99db04974c30cfb6c86d87db58dd5
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-18 17:05:43 +0200

    :fire:

--- LeetCode/Challenges/November/Week 3/MergeIntervals.js
+++ LeetCode/Challenges/November/Week 3/MergeIntervals.js
@@ -37,7 +37,3 @@ var merge = function (intervals) {
   return ans;
 };
 // [[1,3],[2,6],[8,10],[15,18]]
-// if not ans or ans[-1][1] < interval[0]:
-// ans.append(interval)
-// else:
-// ans[-1][1] = max(ans[-1][1], interval[1])
