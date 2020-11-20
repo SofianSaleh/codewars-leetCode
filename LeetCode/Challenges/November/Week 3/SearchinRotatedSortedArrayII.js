c79ceef6630d6e648952487f3ce925aee49cd0d1
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-20 17:32:23 +0200

    :fire:

---
+++ LeetCode/Challenges/November/Week 3/SearchinRotatedSortedArrayII.js
@@ -0,0 +1,43 @@
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+
+// You are given a target value to search. If found in the array return true, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 0
+// Output: true
+// Example 2:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 3
+// Output: false
+// Follow up:
+
+// This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+// Would this affect the run-time complexity? How and why?
+
+// class Solution:
+//     def search(self, nums: List[int], target: int) -> bool:
+//         l = 0
+//         r = len(nums) - 1
+
+//         while l <= r:
+//             m = l + (r - l) // 2
+//             if nums[m] == target:
+//                 return True
+//             if nums[l] == nums[m] == nums[r]:
+//                 l += 1
+//                 r -= 1
+//             elif nums[l] <= nums[m]:
+//                 if nums[l] <= target < nums[m]:
+//                     r = m - 1
+//                 else:
+//                     l = m + 1
+//             else:
+//                 if nums[m] < target <= nums[r]:
+//                     l = m + 1
+//                 else:
+//                     r = m - 1
+
+//         return False
