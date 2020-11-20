fa9e25ff2b90f1335b125aebfa099ab4b8406a62
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-20 17:43:12 +0200

    :fire:

--- LeetCode/Challenges/November/Week 3/SearchinRotatedSortedArrayII.js
+++ LeetCode/Challenges/November/Week 3/SearchinRotatedSortedArrayII.js
@@ -28,18 +28,19 @@ var search = function (nums, target) {
 
   while (l <= r) {
     let m = Math.floor(l + (r - l) / 2);
+
     if (nums[m] == target) return true;
-    if ((nums[l] == nums[m]) == nums[r]) {
+    if (nums[l] == nums[m] && nums[m] == nums[r]) {
       l += 1;
       r -= 1;
     } else if (nums[l] <= nums[m]) {
-      if (nums[l] <= target < nums[m]) {
+      if (nums[l] <= target && target < nums[m]) {
         r = m - 1;
       } else {
         l = m + 1;
       }
     } else {
-      if (nums[m] < target <= nums[r]) {
+      if (nums[m] < target && target <= nums[r]) {
         l = m + 1;
       } else {
         r = m - 1;
