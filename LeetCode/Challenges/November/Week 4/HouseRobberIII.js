45f7c548c8fe6cf4bc23844a945da5b59f009192
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-23 11:42:58 +0200

    :fire:

--- LeetCode/Challenges/November/Week 4/HouseRobberIII.js
+++ LeetCode/Challenges/November/Week 4/HouseRobberIII.js
@@ -39,7 +39,7 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const robR = (root) => {
+const rob = (root) => {
   if (!root) {
     return 0;
   }
@@ -47,37 +47,14 @@ const robR = (root) => {
   let val = 0;
 
   if (root.left) {
-    val += robR(root.left.left) + robR(root.left.right);
+    val += rob(root.left.left) + rob(root.left.right);
   }
 
   if (root.right) {
-    val += robR(root.right.left) + robR(root.right.right);
+    val += rob(root.right.left) + rob(root.right.right);
   }
 
-  return Math.max(val + root.val, robR(root.left) + robR(root.right));
-};
-
-/**
- * Step III - Greedy
- */
-const rob = (root) => {
-  const res = robSub(root);
-  return Math.max(res[0], res[1]);
-};
-
-const robSub = (root) => {
-  if (root == null) {
-    return [0, 0];
-  }
-
-  const left = robSub(root.left);
-  const right = robSub(root.right);
-  const res = [0, 0];
-
-  res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-  res[1] = root.val + left[0] + right[0];
-
-  return res;
+  return Math.max(val + root.val, rob(root.left) + rob(root.right));
 };
 
 // class Solution:
