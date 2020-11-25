f1158cfa28f8657511d3bb5a9b35885052708d28
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-25 11:38:38 +0200

    :fire:

--- LeetCode/Challenges/November/Week 4/SmallestIntegerDivisiblebyK.js
+++ LeetCode/Challenges/November/Week 4/SmallestIntegerDivisiblebyK.js
@@ -35,14 +35,6 @@ var smallestRepunitDivByK = function (K) {
   let N = 0;
 };
 
-// class Solution:
-//     def smallestRepunitDivByK(self, K: int) -> int:
-//         if K % 10 not in {1, 3, 7, 9}:
-//             return -1
-
-//         seen = set()
-//         N = 0
-
 //         for length in range(1, K + 1):
 //             N = (N * 10 + 1) % K
 //             if N == 0:
