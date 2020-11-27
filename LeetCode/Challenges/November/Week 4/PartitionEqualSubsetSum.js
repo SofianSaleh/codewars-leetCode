09268aa2f982bdfc96d754eaae5d61ed931336b8
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-27 11:58:43 +0200

    :fire:

--- LeetCode/Challenges/November/Week 4/PartitionEqualSubsetSum.js
+++ LeetCode/Challenges/November/Week 4/PartitionEqualSubsetSum.js
@@ -66,14 +66,3 @@ console.log(canPartition([1, 5, 11, 5]));
 // class Solution:
 //     def canPartition(self, nums: List[int]) -> bool:
 //         summ = sum(nums)
-
-//         if summ & 1:
-//             return False
-
-//         dp = [True] + [False] * (summ // 2)
-
-//         for num in nums:
-//             for w in range(summ // 2, num - 1, -1):
-//                 dp[w] = dp[w] or dp[w - num]
-
-//         return dp[summ // 2]
