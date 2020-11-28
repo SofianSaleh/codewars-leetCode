296ce08a9b80103f558295e5399bd9ccb3c2b2b6
SofianSaleh <sofiane.saleh11@gmail.com>
2020-11-28 18:13:07 +0200

    :fire:

--- LeetCode/Challenges/November/Week 4/SlidingWindowMaximum.js
+++ LeetCode/Challenges/November/Week 4/SlidingWindowMaximum.js
@@ -44,3 +44,19 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function (nums, k) {};
+
+// class Solution:
+//     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+//         ans = []
+//         q = deque()  # max queue
+
+//         for i, num in enumerate(nums):
+//             while q and q[-1] < num:
+//                 q.pop()
+//             q.append(num)
+//             if i >= k and nums[i - k] == q[0]:  # out of bound
+//                 q.popleft()
+//             if i >= k - 1:
+//                 ans.append(q[0])
+
+//         return ans
