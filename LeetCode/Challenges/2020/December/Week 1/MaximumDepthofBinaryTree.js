7faeb4b1051402860e22d1a8ce2986c591e14da2
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-01 15:43:23 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 1/MaximumDepthofBinaryTree.js
+++ LeetCode/Challenges/2020/December/Week 1/MaximumDepthofBinaryTree.js
@@ -46,3 +46,9 @@ class TreeNode {
  * @return {number}
  */
 var maxDepth = function (root) {};
+
+// class Solution:
+//     def maxDepth(self, root: TreeNode) -> int:
+//         if not root:
+//             return 0
+//         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
