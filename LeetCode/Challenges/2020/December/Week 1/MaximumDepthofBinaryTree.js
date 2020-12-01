069abc213dbca85855151cb5a36e9ca2d63475fe
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-01 15:43:04 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 1/MaximumDepthofBinaryTree.js
+++ LeetCode/Challenges/2020/December/Week 1/MaximumDepthofBinaryTree.js
@@ -23,3 +23,24 @@
 
 // The number of nodes in the tree is in the range [0, 104].
 // -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {};
