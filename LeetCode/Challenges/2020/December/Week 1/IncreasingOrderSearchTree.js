bfa8d2c28a779882aa72f78850a2642188492273
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-03 14:06:45 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 1/IncreasingOrderSearchTree.js
+++ LeetCode/Challenges/2020/December/Week 1/IncreasingOrderSearchTree.js
@@ -47,11 +47,6 @@ var increasingBST = function (root) {
   return recurse(root, null);
 };
 
-// class Solution:
-//     def increasingBST(self, root: TreeNode, tail: TreeNode = None) -> TreeNode:
-//         if not root:
-//             return tail
-
 //         res = self.increasingBST(root.left, root)
 //         root.left = None
 //         root.right = self.increasingBST(root.right, tail)
