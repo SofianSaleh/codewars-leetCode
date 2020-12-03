98e49fa5b0f6307a4d664ac659c8a88b7ecac627
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-03 14:01:57 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 1/IncreasingOrderSearchTree.js
+++ LeetCode/Challenges/2020/December/Week 1/IncreasingOrderSearchTree.js
@@ -13,3 +13,17 @@
 
 // The number of nodes in the given tree will be in the range [1, 100].
 // 0 <= Node.val <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function (root) {};
