bf4b0f0ce34de05e254684125c00d3da021098c9
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-03 14:02:47 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 1/IncreasingOrderSearchTree.js
+++ LeetCode/Challenges/2020/December/Week 1/IncreasingOrderSearchTree.js
@@ -28,10 +28,10 @@ class TreeNode {
     this.val = val === undefined ? 0 : val;
     this.left = left === undefined ? null : left;
     this.right = right === undefined ? null : right;
-    /**
-     * @param {TreeNode} root
-     * @return {TreeNode}
-     */
-    var increasingBST = function (root) {};
   }
 }
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function (root) {};
