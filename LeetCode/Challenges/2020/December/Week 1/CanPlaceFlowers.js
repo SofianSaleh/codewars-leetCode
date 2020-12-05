dafd68da063868f5028222ed97be82b72f2f897e
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-05 15:38:19 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 1/CanPlaceFlowers.js
+++ LeetCode/Challenges/2020/December/Week 1/CanPlaceFlowers.js
@@ -50,6 +50,3 @@ var canPlaceFlowers = function (flowerbed, n) {
 
 //     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
 //         for i, flower in enumerate(flowerbed):
-//             if flower == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
-//                 flowerbed[i] = 1
-/
