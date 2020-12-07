62979734950eeb0d0ea4df06c7e6e91b9eefbd92
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-07 13:42:35 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 1/SpiralMatrixII.js
+++ LeetCode/Challenges/2020/December/Week 1/SpiralMatrixII.js
@@ -39,7 +39,7 @@ var generateMatrix = function (n) {
       ans[i][max] = count;
       count += 1;
     }
-    for (let i = max; i <= min; i--) {
+    for (let i = max; i < min; i--) {
       ans[max][i] = count;
       count += 1;
     }
@@ -48,7 +48,7 @@ var generateMatrix = function (n) {
       count += 1;
     }
   }
-  if (n && 1) {
+  if (n % 2 == 1) {
     ans[Math.floor(n / 2)][Math.floor(n / 2)] = count;
   }
   return ans;
@@ -57,10 +57,6 @@ var generateMatrix = function (n) {
 console.log(generateMatrix(3));
 
 //         for min in range(n // 2):
-//             max = n - min - 1
-//             for i in range(min, max):
-//                 ans[i][max] = count
-//                 count += 1
 //             for i in range(max, min, -1):
 //                 ans[max][i] = count
 //                 count += 1
