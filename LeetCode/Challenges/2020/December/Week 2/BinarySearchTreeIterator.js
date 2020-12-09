1f7ad77ff67ed35cb6a253e84562ddb8da40a4cb
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-09 11:37:43 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 2/BinarySearchTreeIterator.js
+++ LeetCode/Challenges/2020/December/Week 2/BinarySearchTreeIterator.js
@@ -36,3 +36,33 @@
 // Follow up:
 
 // Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory, where h is the height of the tree?
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function (root) {};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function () {};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function () {};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
