f630c16427bab5db690bc33fef73e4c395cedd76
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-09 11:41:27 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 2/BinarySearchTreeIterator.js
+++ LeetCode/Challenges/2020/December/Week 2/BinarySearchTreeIterator.js
@@ -95,8 +95,3 @@ class BSTIterator {
 
 //     def hasNext(self) -> bool:
 //         return self.stack
-
-//     def pushLeftsUntilNone(self, root: TreeNode):
-//         while root:
-//             self.stack.append(root)
-//             root = root.left
