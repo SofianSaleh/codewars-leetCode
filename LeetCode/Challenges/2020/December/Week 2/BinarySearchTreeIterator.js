c48f44d68449aabef99aea95c20eba51856ab73a
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-09 11:38:29 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 2/BinarySearchTreeIterator.js
+++ LeetCode/Challenges/2020/December/Week 2/BinarySearchTreeIterator.js
@@ -1,11 +1,17 @@
-// Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+// Implement the BSTIterator class that represents an iterator over the in-order traversal
+// of a binary search tree (BST):
 
-// BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
-// boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+// BSTIterator(TreeNode root) Initializes an object of the BSTIterator class.
+// The root of the BST is given as part of the constructor.
+// The pointer should be initialized to a non-existent number smaller than any element in the BST.
+// boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer,
+// otherwise returns false.
 // int next() Moves the pointer to the right, then returns the number at the pointer.
-// Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+// Notice that by initializing the pointer to a non-existent smallest number,
+// the first call to next() will return the smallest element in the BST.
 
-// You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+// You may assume that next() calls will always be valid. That is,
+// there will be at least a next number in the in-order traversal when next() is called.
 
 // Example 1:
 
@@ -66,3 +72,21 @@ BSTIterator.prototype.hasNext = function () {};
  * var param_1 = obj.next()
  * var param_2 = obj.hasNext()
  */
+
+// class BSTIterator:
+//     def __init__(self, root: TreeNode):
+//         self.stack = []
+//         self.pushLeftsUntilNone(root)
+
+//     def next(self) -> int:
+//         root = self.stack.pop()
+//         self.pushLeftsUntilNone(root.right)
+//         return root.val
+
+//     def hasNext(self) -> bool:
+//         return self.stack
+
+//     def pushLeftsUntilNone(self, root: TreeNode):
+//         while root:
+//             self.stack.append(root)
+//             root = root.left
