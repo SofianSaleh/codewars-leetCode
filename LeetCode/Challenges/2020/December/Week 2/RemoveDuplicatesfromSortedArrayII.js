696cdfe13a4df1cbb62e962cfd0481ca5dc6ad9a
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-11 12:07:55 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 2/RemoveDuplicatesfromSortedArrayII.js
+++ LeetCode/Challenges/2020/December/Week 2/RemoveDuplicatesfromSortedArrayII.js
@@ -43,7 +43,9 @@
 var removeDuplicates = function (nums) {
   let i = 0;
   for (const num of nums) {
-      if(i < 2 || num != nums[i-2])
+    if (i < 2 || num != nums[i - 2]) {
+      nums[i] = num;
+    }
   }
 };
 
