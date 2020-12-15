61455d80179c4288f655dc21e18c378782ee4d33
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-15 11:23:05 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 3/SquaresofaSortedArray.js
+++ LeetCode/Challenges/2020/December/Week 3/SquaresofaSortedArray.js
@@ -16,3 +16,9 @@
 // 1 <= nums.length <= 104
 // -104 <= nums[i] <= 104
 // nums is sorted in non-decreasing order.
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {};
