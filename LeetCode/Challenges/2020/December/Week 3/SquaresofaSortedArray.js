8878d9c0b9499f2e7d1e19e1a17d273fc16a5cae
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-15 11:25:14 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 3/SquaresofaSortedArray.js
+++ LeetCode/Challenges/2020/December/Week 3/SquaresofaSortedArray.js
@@ -21,4 +21,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function (nums) {};
+var sortedSquares = function (nums) {
+  let n = nums.length;
+};
+
+// class Solution:
+//     def sortedSquares(self, A: List[int]) -> List[int]:
+//         n = len(A)
+//         l = 0
+//         r = n - 1
+//         ans = [0] * n
+
+//         while n:
+//             n -= 1
+//             if abs(A[l]) > abs(A[r]):
+//                 ans[n] = A[l] * A[l]
+//                 l += 1
+//             else:
+//                 ans[n] = A[r] * A[r]
+//                 r -= 1
+
+//         return ans
