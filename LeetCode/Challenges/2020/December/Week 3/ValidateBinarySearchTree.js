064d1df1685e09b5625ea09157e126fe96359abb
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-16 12:52:28 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 3/ValidateBinarySearchTree.js
+++ LeetCode/Challenges/2020/December/Week 3/ValidateBinarySearchTree.js
@@ -29,6 +29,13 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+* function TreeNode(val, left, right) {
+    *     this.val = (val===undefined ? 0 : val)
+    *     this.left = (left===undefined ? null : left)
+    *     this.right = (right===undefined ? null : right)
+    * }
+
 /**
  * @param {TreeNode} root
  * @return {boolean}
