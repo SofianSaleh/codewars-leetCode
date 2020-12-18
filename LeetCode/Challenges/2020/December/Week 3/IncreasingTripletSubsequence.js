2636e13ad9323df2bdd4ff76f3b4a59b016001c8
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-18 11:43:06 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 3/IncreasingTripletSubsequence.js
+++ LeetCode/Challenges/2020/December/Week 3/IncreasingTripletSubsequence.js
@@ -41,6 +41,6 @@ var increasingTriplet = function (nums) {
 };
 
 console.log(increasingTriplet([1, 2, 3, 4, 5]));
-console.log(increasingTriplet([5, 4, 3, 2, 1]));
+// console.log(increasingTriplet([5, 4, 3, 2, 1]));
 // console.log(increasingTriplet([2, 1, 5, 0, 4, 6]));
 // console.log(increasingTriplet([5, 1, 5, 5, 2, 5, 4]));
