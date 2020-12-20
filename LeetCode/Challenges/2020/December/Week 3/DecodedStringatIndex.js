3b22b646045a999a77c5a5afb55f5d75b1f1e667
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-20 13:05:50 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 3/DecodedStringatIndex.js
+++ LeetCode/Challenges/2020/December/Week 3/DecodedStringatIndex.js
@@ -72,7 +72,7 @@ var decodeAtIndex = function (S, K) {
   return S[K - 1];
 };
 
-console.log(decodeAtIndex('leet2code3', 10));
+// console.log(decodeAtIndex('leet2code3', 10));
 // console.log(decodeAtIndex('ha22', 5));
 // console.log(decodeAtIndex('a2345678999999999999999', 1));
 // console.log(decodeAtIndex('a2b3c4d5e6f7g8h9', 9));
