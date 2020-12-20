98e83145798bc204863424dac1f45aa7ebbf871e
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-20 13:02:46 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 3/DecodedStringatIndex.js
+++ LeetCode/Challenges/2020/December/Week 3/DecodedStringatIndex.js
@@ -58,6 +58,7 @@ var decodeAtIndex = function (S, K) {
           stack.push(x);
         }
         let y = stack.join('');
+        console.log(y);
         return y[K - 1];
       }
       for (let i = 0; i < c; i++) {
@@ -68,9 +69,10 @@ var decodeAtIndex = function (S, K) {
       currStr += c;
     }
   }
-  return stack;
+  return S[K - 1];
 };
 
 // console.log(decodeAtIndex('leet2code3', 10));
 // console.log(decodeAtIndex('ha22', 5));
-console.log(decodeAtIndex('a2345678999999999999999', 1));
+// console.log(decodeAtIndex('a2345678999999999999999', 1));
+console.log(decodeAtIndex('a2b3c4d5e6f7g8h9', 9));
