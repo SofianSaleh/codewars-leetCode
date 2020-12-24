8bf30624a3439de1f19be66cfd2b4852746ddf12
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-24 15:26:29 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 4/SwapNodesinPairs.js
+++ LeetCode/Challenges/2020/December/Week 4/SwapNodesinPairs.js
@@ -40,3 +40,26 @@ class ListNode {
  * @return {ListNode}
  */
 var swapPairs = function (head) {};
+
+// class Solution:
+//     def swapPairs(self, head: ListNode) -> ListNode:
+//         dummy = ListNode(0)
+//         dummy.next = head
+//         length = 0
+//         curr = head
+//         while curr:
+//             length += 1
+//             curr = curr.next
+
+//         prev = dummy
+//         curr = head
+
+//         for i in range(length // 2):
+//             next = curr.next
+//             curr.next = next.next
+//             next.next = prev.next
+//             prev.next = next
+//             prev = curr
+//             curr = curr.next
+
+//         return dummy.next
