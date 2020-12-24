72bb8ad1dfb8f4a0bed89488a48610ebe513f9e0
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-24 15:25:05 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 4/SwapNodesinPairs.js
+++ LeetCode/Challenges/2020/December/Week 4/SwapNodesinPairs.js
@@ -30,12 +30,12 @@
  * }
  */
 
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
+  function ListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
 
-/**
+/** */
  * @param {ListNode} head
  * @return {ListNode}
  */
