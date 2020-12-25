49527fca6eb8a6f1ef7adf62b564eeb36100f715
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-25 12:36:21 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 4/DiagonalTraverse.js
+++ LeetCode/Challenges/2020/December/Week 4/DiagonalTraverse.js
@@ -41,28 +41,19 @@ var findDiagonalOrder = function (matrix) {
       col += 2;
       d = -d;
     }
+    if (row < 0) {
+      row = 0;
+      d = -d;
+    }
+    if (col < 0) {
+      col = 0;
+      d = -d;
+    }
   }
 };
 
 //       for (int i = 0; i < m * n; ++i) {
-//         ans[i] = matrix[row][col];
-//         row -= d;
-//         col += d;
-//         // out-of-bound
-//         if (row == m) {
-//           row = m - 1;
-//           col += 2;
-//           d = -d;
-//         }
-//         if (col == n) {
-//           col = n - 1;
-//           row += 2;
-//           d = -d;
-//         }
-//         if (row < 0) {
-//           row = 0;
-//           d = -d;
-//         }
+
 //         if (col < 0) {
 //           col = 0;
 //           d = -d;
