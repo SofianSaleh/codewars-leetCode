34ef354e8ec499d4b666462c946b158ca15a0b60
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-26 12:55:30 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 4/DecodeWays.js
+++ LeetCode/Challenges/2020/December/Week 4/DecodeWays.js
@@ -38,11 +38,41 @@
  * @return {number}
  */
 var numDecodings = function (s) {
-  let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let obj = {};
-  for (let i = 1; i <= 26; i++) {
-    obj[str[i - 1]] = i;
-  }
-  console.log(obj);
+  //   let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  //   let obj = {};
+  //   for (let i = 1; i <= 26; i++) {
+  //     obj[str[i - 1]] = i;
+  //   }
+  //   var decode = function (num,i) {
+  //       if()
+  //   }
 };
 console.log(numDecodings('123'));
+
+// class Solution {
+//     public int numDecodings(String s) {
+//       final int n = s.length();
+
+//       // dp[i] := # of ways to decode s[i..n)
+//       int[] dp = new int[n + 1];
+//       dp[n] = 1; // ""
+//       dp[n - 1] = isValid(s.charAt(n - 1)) ? 1 : 0;
+
+//       for (int i = n - 2; i >= 0; --i) {
+//         if (isValid(s.charAt(i)))
+//           dp[i] += dp[i + 1];
+//         if (isValid(s.charAt(i), s.charAt(i + 1)))
+//           dp[i] += dp[i + 2];
+//       }
+
+//       return dp[0];
+//     }
+
+//     private boolean isValid(char c) {
+//       return '1' <= c && c <= '9';
+//     }
+
+//     private boolean isValid(char c1, char c2) {
+//       return c1 == '1' || (c1 == '2' && c2 <= '6');
+//     }
+//   }
