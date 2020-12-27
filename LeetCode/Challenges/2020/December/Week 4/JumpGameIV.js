b8b056e3a80726d2cd91b2401605332f1d3e9dfe
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-27 14:57:35 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 4/JumpGameIV.js
+++ LeetCode/Challenges/2020/December/Week 4/JumpGameIV.js
@@ -44,44 +44,23 @@
  */
 var minJumps = function (arr) {
   if (arr.length == 1) return 0;
-  let count = 0;
-  let len = arr.length;
-  let graph = {};
-
-  for (let i = 0; i < len; i++) {
-    if (!graph[arr[i]]) {
-      graph[arr[i]] = i;
-    }
-  }
-  let visited = [];
-  visited[0] = true;
-  let queue = [];
-  while (!queue.length == 0) {
-    let size = queue.length;
-    for (let i=size; i>0; i--) {
-        let current = queue.poll();
-                if (current == len - 1)
-                    return count;
-                let next = graph[arr[current]];
-                next.add(current-1);
-                next.add(current+1);
-                for(const j of next){
-                    if (j >= 0 && j<len && !visited[j]) {
-                        visited[j] = true;
-                        queue.offer(j);
-                    }
-                }
-                next.clear();
-            }
-            count += 1;
-        }
-        return 0;
-    }
 };
-
+// class Solution {
+//     public int minJumps(int[] arr) {
+//         if (arr.length == 1)
+//             return 0;
+//         int count = 0;
+//         int length = arr.length;
+//         Map<Integer, List<Integer>> graph = new HashMap<>();
+//         // populate the graph with index
 //         for (int i=0; i<length; i++)
 //             graph.computeIfAbsent(arr[i], x->new LinkedList<>()).add(i);
 //         // create the boolean array visited
+//         boolean [] visited = new boolean [length];
+//         // mark the first element as visited
+//         visited[0] = true;
+//         // since we do bfs, we need a queue
+//         Queue<Integer> queue = new LinkedList<>();
 //         queue.offer(0);
 //         while (!queue.isEmpty()) {
 //             int size = queue.size();
