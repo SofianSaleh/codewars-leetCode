7baf9d21d21d534ebea99b98189ab27c41fb30a6
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-27 14:57:52 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 4/JumpGameIV.js
+++ LeetCode/Challenges/2020/December/Week 4/JumpGameIV.js
@@ -46,16 +46,6 @@ var minJumps = function (arr) {
   if (arr.length == 1) return 0;
 };
 
-//         for (int i=0; i<length; i++)
-//             graph.computeIfAbsent(arr[i], x->new LinkedList<>()).add(i);
-//         // create the boolean array visited
-//         boolean [] visited = new boolean [length];
-//         // mark the first element as visited
-//         visited[0] = true;
-//         // since we do bfs, we need a queue
-//         Queue<Integer> queue = new LinkedList<>();
-//         queue.offer(0);
-//         while (!queue.isEmpty()) {
 //             int size = queue.size();
 //             for (int i=size; i>0; i--) {
 //                 int current = queue.poll();
