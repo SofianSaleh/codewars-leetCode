4134b45ffef40afd10fe4c6cd892f62e527b0118
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-29 16:32:06 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 5/Pseudo-PalindromicPathsinaBinaryTree.js
+++ LeetCode/Challenges/2020/December/Week 5/Pseudo-PalindromicPathsinaBinaryTree.js
@@ -52,14 +52,7 @@ var pseudoPalindromicPaths = function (root) {
   //       path ^= 1 << root.val;
   //       if (path && path - 1 == 0) {
   //         ans += 1;
-  //         return;
-  //       }
-  //       dfs(root.left, path ^ (1 << root.val));
-  //       dfs(root.right, path ^ (1 << root.val));
-  //     }
-  //   };
-  //   dfs(root, 0);
-  //   return ans;
+
   var dfs = function (root, path) {
     if (root == null) return;
     if (root.left == null && root.right == null) {
