5f30d25bc3057ed382cbcfa100d120b55d4f9d5c
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-30 17:59:49 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 5/GameofLife.js
+++ LeetCode/Challenges/2020/December/Week 5/GameofLife.js
@@ -62,13 +62,6 @@ var gameOfLife = function (board) {
   return board;
 };
 
-//         for i in range(m):
-//             for j in range(n):
-//                 ones = 0
-//                 for y in range(max(0, i - 1), min(m, i + 2)):
-//                     for x in range(max(0, j - 1), min(n, j + 2)):
-//                         ones += board[y][x] & 1
-//                 # any live cell with 2 or 3 live neighbors
 //                 # lives on to the next generation
 //                 if (board[i][j] == 1 and (ones == 3 or ones == 4)):
 //                     board[i][j] |= 0b10
