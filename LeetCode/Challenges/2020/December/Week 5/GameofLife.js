2ae23e76fe73a4bd5b9b448ac533aaa90113db5e
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-30 18:04:42 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 5/GameofLife.js
+++ LeetCode/Challenges/2020/December/Week 5/GameofLife.js
@@ -37,26 +37,47 @@ var gameOfLife = function (board) {
   let m = board.length;
   let n = board[0].length;
 
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; i++) {
+  for (let i = 0; i < m; ++i) {
+    for (let j = 0; j < n; ++j) {
       let ones = 0;
 
-      for (let y = Math.max(0, i - 1); y < Math.min(m, i + 2); y++) {
-        for (let x = Math.max(0, j - 1); x < Math.min(n, j + 2); i++) {
-          ones += board[y][x] && 1;
+      for (let y = Math.max(0, i - 1); y < Math.min(m, i + 2); ++y) {
+        for (let x = Math.max(0, j - 1); x < Math.min(n, j + 2); ++x) {
+          ones += board[y][x] & 1;
         }
       }
-    }
-    if (board[i][j] == 1 && (ones == 3 || ones == 4)) {
-      board[i][j] |= 0b10;
-    }
-    if (board[i][j] == 0 && ones == 3) {
-      board[i][j] |= 0b10;
+      if (board[i][j] == 1 && (ones == 3 || ones == 4)) board[i][j] |= 0b10;
+      if (board[i][j] == 0 && ones == 3) board[i][j] |= 0b10;
     }
   }
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
+  for (let i = 0; i < m; ++i) {
+    for (let j = 0; j < n; ++j) {
       board[i][j] >>= 1;
     }
   }
 };
+
+// class Solution {
+//   public void gameOfLife(int[][] board) {
+//     final int m = board.length;
+//     final int n = board[0].length;
+
+//     for (int i = 0; i < m; ++i)
+//       for (int j = 0; j < n; ++j) {
+//         int ones = 0;
+//         for (int y = Math.max(0, i - 1); y < Math.min(m, i + 2); ++y)
+//           for (int x = Math.max(0, j - 1); x < Math.min(n, j + 2); ++x)
+//             ones += board[y][x] & 1;
+//         // any live cell with 2 or 3 live neighbors lives on to the next generation
+//         if (board[i][j] == 1 && (ones == 3 || ones == 4))
+//           board[i][j] |= 0b10;
+//         // any dead cell with exactly 3 live neighbors becomes a live cell, as if by reproduction
+//         if (board[i][j] == 0 && ones == 3)
+//           board[i][j] |= 0b10;
+//       }
+
+//     for (int i = 0; i < m; ++i)
+//       for (int j = 0; j < n; ++j)
+//         board[i][j] >>= 1;
+//   }
+// }
