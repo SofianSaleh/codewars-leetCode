3d6314222e8718f079a70875b89fe931105c6fe8
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-31 12:19:03 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 5/LargestRectangleinHistogram.js
+++ LeetCode/Challenges/2020/December/Week 5/LargestRectangleinHistogram.js
@@ -34,11 +34,4 @@ var largestRectangleArea = function (heights) {
 
 console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]));
 
-//         for i in range(len(heights) + 1):
-//             while stack != [] and (i == len(heights) or heights[i] < heights[stack[-1]]):
-//                 h = heights[stack.pop()]
-//                 w = i if stack == [] else i - stack[-1] - 1
-//                 ans = max(ans, h * w)
-//             stack.append(i)
-
 //         return ans
