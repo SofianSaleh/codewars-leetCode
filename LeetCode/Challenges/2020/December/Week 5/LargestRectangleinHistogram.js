b9f446bd67a16618641d7711f0a9b16dc5fc9b48
SofianSaleh <sofiane.saleh11@gmail.com>
2020-12-31 12:11:38 +0200

    :fire:

--- LeetCode/Challenges/2020/December/Week 5/LargestRectangleinHistogram.js
+++ LeetCode/Challenges/2020/December/Week 5/LargestRectangleinHistogram.js
@@ -18,13 +18,29 @@ var largestRectangleArea = function (heights) {
 
   for (let i = 0; i < heights.length; i++) {
     for (let j = i + 1; j < heights.length; j++) {
-      let div = Math.abs(heights[i] - heights[j]);
+      let div = Math.abs(heights[i] - heights[j]) * Math.abs(i - j);
+      console.log(i, j);
       if (div > ans) {
         ans = div;
       }
+      break;
     }
   }
   return ans;
 };
 
 console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]));
+
+// class Solution:
+//     def largestRectangleArea(self, heights: List[int]) -> int:
+//         ans = 0
+//         stack = []
+
+//         for i in range(len(heights) + 1):
+//             while stack != [] and (i == len(heights) or heights[i] < heights[stack[-1]]):
+//                 h = heights[stack.pop()]
+//                 w = i if stack == [] else i - stack[-1] - 1
+//                 ans = max(ans, h * w)
+//             stack.append(i)
+
+//         return ans
