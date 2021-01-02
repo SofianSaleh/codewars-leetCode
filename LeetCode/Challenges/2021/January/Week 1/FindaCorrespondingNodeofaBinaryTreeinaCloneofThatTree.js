a6fd9e03287da52190c3bff43f7d6f695684cf1f
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-02 13:10:01 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 1/FindaCorrespondingNodeofaBinaryTreeinaCloneofThatTree.js
+++ LeetCode/Challenges/2021/January/Week 1/FindaCorrespondingNodeofaBinaryTreeinaCloneofThatTree.js
@@ -59,3 +59,14 @@ class TreeNode {
  */
 
 var getTargetCopy = function (original, cloned, target) {};
+
+// class Solution {
+//     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+//             if (original == null || original == target)
+//                 return cloned;
+//             TreeNode res = getTargetCopy(original.left, cloned.left, target);
+//             if (res != null)
+//                 return res;
+//             return getTargetCopy(original.right, cloned.right, target);
+//         }
+// }
