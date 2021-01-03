f0341e417b45e093b8af06f27aba1e8940ba9352
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-03 13:13:16 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 1/BeautifulArrangement.js
+++ LeetCode/Challenges/2021/January/Week 1/BeautifulArrangement.js
@@ -34,35 +34,27 @@ var countArrangement = function (n) {
     let filled = sb.toString();
     if (filled in memo) return memo[filled];
     let count = 0;
+
+    for (let i = 0; i < N; ++i) {
+      if (sb.charAt(i) === 'x' && (num % i === 0 || i % num === 0)) {
+        sb.setCharAt(i, 'o');
+        count += dfs(N, num + 1, sb, memo);
+        sb.setCharAt(i, 'x');
+      }
+    }
+    memo[filled] = count;
+    return count;
   };
+  let filled = new String(new char[N + 1]()).replace('\0', 'x');
+  let sb = filled;
+  let memo = {};
+
+  return dfs(N, 1, sb, memo);
 };
 
 // class Solution {
 //     public int countArrangement(int N) {
-//       final String filled = new String(new char[N + 1]).replace('\0', 'x');
-//       StringBuilder sb = new StringBuilder(filled);
-//       Map<String, Integer> memo = new HashMap<>();
 
-//       return dfs(N, 1, sb, memo);
 //     }
 
-//     private int dfs(int N, int num, StringBuilder sb, Map<String, Integer> memo) {
-//       if (num == N + 1)
-//         return 1;
-//       final String filled = sb.toString();
-//       if (memo.containsKey(filled))
-//         return memo.get(filled);
-
-//       int count = 0;
-
-//       for (int i = 1; i <= N; ++i)
-//         if (sb.charAt(i) == 'x' && (num % i == 0 || i % num == 0)) {
-//           sb.setCharAt(i, 'o');
-//           count += dfs(N, num + 1, sb, memo);
-//           sb.setCharAt(i, 'x');
-//         }
-
-//       memo.put(filled, count);
-//       return count;
-//     }
 //   }
