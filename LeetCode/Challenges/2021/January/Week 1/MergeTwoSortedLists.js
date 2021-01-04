60b07a2f0fb75b8fee901395071f6ab44b4d8667
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-04 15:34:08 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 1/MergeTwoSortedLists.js
+++ LeetCode/Challenges/2021/January/Week 1/MergeTwoSortedLists.js
@@ -18,3 +18,17 @@
 // The number of nodes in both lists is in the range [0, 50].
 // -100 <= Node.val <= 100
 // Both l1 and l2 are sorted in non-decreasing order.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {};
