5156297666d79aaee428372d0e499b588b51d625
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-04 15:43:08 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 1/MergeTwoSortedLists.js
+++ LeetCode/Challenges/2021/January/Week 1/MergeTwoSortedLists.js
@@ -41,7 +41,15 @@ let l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function (l1, l2) {};
+var mergeTwoLists = function (l1, l2) {
+  if (!l1 || !l2) {
+    return l1 ? l1 : l2;
+  }
+  if (l1.val > l2.val) [l1, l2] = [l2, l1];
+  l1.next = self.mergeTwoLists(l1.next, l2);
+
+  return l1;
+};
 
 // class Solution:
 //     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
