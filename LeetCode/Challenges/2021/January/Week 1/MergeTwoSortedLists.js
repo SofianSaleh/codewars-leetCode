173d757a496b20f36dfd71f40077ef0e045d283c
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-04 15:44:56 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 1/MergeTwoSortedLists.js
+++ LeetCode/Challenges/2021/January/Week 1/MergeTwoSortedLists.js
@@ -27,12 +27,12 @@
  * }
  */
 
-class ListNode {
-  constructor(val, next) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+// class ListNode {
+//   constructor(val, next) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
 
 // let l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 // let l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
