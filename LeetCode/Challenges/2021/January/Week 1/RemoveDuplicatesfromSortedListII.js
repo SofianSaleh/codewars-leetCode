0d5f9ef485eae36994a257eda9df09d9dfe03643
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-05 13:32:08 +0200

    :rocket:

--- LeetCode/Challenges/2021/January/Week 1/RemoveDuplicatesfromSortedListII.js
+++ LeetCode/Challenges/2021/January/Week 1/RemoveDuplicatesfromSortedListII.js
@@ -14,3 +14,16 @@
 // The number of nodes in the list is in the range [0, 300].
 // -100 <= Node.val <= 100
 // The list is guaranteed to be sorted in ascending order.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {};
