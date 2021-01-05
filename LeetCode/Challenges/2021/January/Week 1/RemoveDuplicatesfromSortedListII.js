c41b8302c2890634aee85707a62883787595b8da
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-05 13:50:21 +0200

    :rocket:

--- LeetCode/Challenges/2021/January/Week 1/RemoveDuplicatesfromSortedListII.js
+++ LeetCode/Challenges/2021/January/Week 1/RemoveDuplicatesfromSortedListII.js
@@ -23,13 +23,35 @@
  * }
  */
 
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
+// [1,2,3,3,4,4,5]
+let x = new ListNode(
+  1,
+  new ListNode(
+    2,
+    new ListNode(
+      3,
+      new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5))))
+    )
+  )
+);
 
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function (head) {};
+var deleteDuplicates = function (head) {
+  let newList = new ListNode(0);
+  newList.next = head;
+  let prev = head;
+  while (head) {
+    while ((head.next && head.val) === head.next.val) {
+      head = head.next;
+    }
+  }
+};
