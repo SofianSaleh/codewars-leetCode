5868b2a64cfbf6d6db5bd58eaddf6035f641ae29
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-07 11:52:15 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 1/LongestSubstringWithoutRepeatingCharacters.py
+++ LeetCode/Challenges/2021/January/Week 1/LongestSubstringWithoutRepeatingCharacters.py
@@ -24,31 +24,21 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        count = 0
-        str = ''
-
-        for string in s:
-            if string not in str:
-                str += string
-
-            else:
-                if len(str) > count:
-                    count = len(str)
-                str = ''
-                str += string
-                print(str)
-        if len(str) > count:
-            count = len(str)
+        count = Counter()
+        ans = 0
+        l = 0
+        for r, c in enumerate(s):
+            count[c] += 1
+            while count[c] > 1:
+                count[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
         return count
 
 
 # class Solution:
 #     def lengthOfLongestSubstring(self, s: str) -> int:
-#         ans = 0
-#         count = Counter()
 
-#         l = 0
-#         for r, c in enumerate(s):
 #             count[c] += 1
 #             while count[c] > 1:
 #                 count[s[l]] -= 1
