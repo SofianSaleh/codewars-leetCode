8b7cfa43e874e47c41fb870621cd5458a1de0919
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-08 18:19:52 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 2/CheckIfTwoStringArraysareEquivalent.py
+++ LeetCode/Challenges/2021/January/Week 2/CheckIfTwoStringArraysareEquivalent.py
@@ -36,6 +36,6 @@ class Solution:
 
 
 x = Solution()
-print(x.arrayStringsAreEqual(word1=["ab", "c"], word2=["a", "bc"]))
+# print(x.arrayStringsAreEqual(word1=["ab", "c"], word2=["a", "bc"]))
 # print(x.arrayStringsAreEqual(word1=["a", "cb"], word2=["ab", "c"]))
 # print(x.arrayStringsAreEqual(word1=["abc", "d", "defg"], word2=["abcddefg"]))
