60d0cf5a889d754f1472fd3946217f5b366052c0
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-09 13:09:51 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 2/WordLadder.py
+++ LeetCode/Challenges/2021/January/Week 2/WordLadder.py
@@ -52,3 +52,5 @@ x = Solution()
 
 print(x.ladderLength(beginWord="hit", endWord="cog", wordList=[
       "hot", "dot", "dog", "lot", "log", "cog"]))  # 5
+
+print(x.ladderLength(beginWord="hit", endWord="cog", wordList=["hot", "dot", "dog", "lot", "log"])  # 0
