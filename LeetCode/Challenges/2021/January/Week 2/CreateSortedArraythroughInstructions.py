8ca351502267ba6e6ce69a0565be7294f6435c34
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-10 13:09:10 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 2/CreateSortedArraythroughInstructions.py
+++ LeetCode/Challenges/2021/January/Week 2/CreateSortedArraythroughInstructions.py
@@ -55,3 +55,37 @@
 class Solution:
     def createSortedArray(self, instructions):
         pass
+
+
+def createSorted(a: list, n: int):
+
+    # Auxiliary Array
+    b = []
+    for j in range(n):
+
+        if len(b) == 0:
+            b.append(a[j])
+        else:
+            start = 0
+            end = len(b) - 1
+
+            pos = 0
+            while start <= end:
+                mid = start + (end - start) // 2
+
+                if b[mid] == a[j]:
+
+                    b.insert(max(0, mid + 1), a[j])
+                    break
+
+                elif b[mid] > a[j]:
+
+                    pos = end = mid - 1
+                else:
+
+                    pos = start = mid + 1
+
+                if start > end:
+                    pos = start
+                    b.insert(max(0, pos), a[j])
+                    break
