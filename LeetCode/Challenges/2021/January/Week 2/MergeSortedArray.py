b7e90a3a48bc51c22bcbd99780704f708cd3f69e
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-11 13:43:56 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 2/MergeSortedArray.py
+++ LeetCode/Challenges/2021/January/Week 2/MergeSortedArray.py
@@ -23,7 +23,7 @@
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m, nums2, n):
         k = m + n
 
         while n > 0:
@@ -36,7 +36,7 @@ class Solution:
                 nums1[k] = nums2[n]
 
 
-x = Solution()
+# x = Solution()
 # print(x.merge([1, 2, 3, 0, 0, 0]
 #   3,
 #   [2, 5, 6],
