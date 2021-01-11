72fa65cb693fde9dca61870840705653762acc33
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-11 13:42:49 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 2/MergeSortedArray.py
+++ LeetCode/Challenges/2021/January/Week 2/MergeSortedArray.py
@@ -23,17 +23,22 @@
 
 
 class Solution:
-    def merge(self, nums1, m, nums2, n) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        nums1 = nums1[:m]
-        nums2 = nums2[:n]
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        k = m + n
 
-        nums1 += nums2
-        nums1.sort()
-        return nums1
+        while n > 0:
+            k -= 1
+            if m > 0 and nums1[m - 1] > nums2[n - 1]:
+                m -= 1
+                nums1[k] = nums1[m]
+            else:
+                n -= 1
+                nums1[k] = nums2[n]
 
 
 x = Solution()
-print(x.merge(nums1=[1, 2, 3, 0, 0, 0], m=3, nums2=[2, 5, 6], n=3))
+print(x.merge([1, 2, 3, 0, 0, 0]
+              3,
+              [2, 5, 6],
+              3))  # [1,2,2,3,5,6]
+print(x.merge(nums1=[1], m=1, nums2=[], n=0))  # [1]
