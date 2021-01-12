bf74ed2014ceaf70bd518a272b0498ec4db38410
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-12 10:59:41 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 2/AddTwoNumbers.py
+++ LeetCode/Challenges/2021/January/Week 2/AddTwoNumbers.py
@@ -31,5 +31,13 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1, l2):
+        pass
