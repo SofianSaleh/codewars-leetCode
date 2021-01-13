f20ade91a6d196d51cc38132ca5887019abc3b76
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-13 14:30:14 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 2/BoatstoSavePeople.py
+++ LeetCode/Challenges/2021/January/Week 2/BoatstoSavePeople.py
@@ -52,7 +52,23 @@ class Solution:
                         break
             else:
                 count += 1
-            i += 1
+
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        ans = 0
+        i = 0
+        j = len(people) - 1
+
+        people.sort()
+
+        while i <= j:
+            remain = limit - people[j]
+            j -= 1
+            if people[i] <= remain:
+                i += 1
+            ans += 1
+
+        return ans
+
         return count
 
 
