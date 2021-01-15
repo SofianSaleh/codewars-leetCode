97b1c76d8d9d65f2a46936400ac752888aed39d0
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-15 15:21:31 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/GetMaximuminGeneratedArray.py
+++ LeetCode/Challenges/2021/January/Week 3/GetMaximuminGeneratedArray.py
@@ -51,7 +51,7 @@ class Solution(object):
             for i in range(len(nums), n+1):
                 if i % 2 == 0:
                     nums.append(nums[i//2])
-        #         else:
-        #             nums.append(nums[i//2] + nums[i//2+1])
-        #         dp.append(max(dp[-1], nums[-1]))
-        # return dp[n]
+                else:
+                    #             nums.append(nums[i//2] + nums[i//2+1])
+                    #         dp.append(max(dp[-1], nums[-1]))
+                    # return dp[n]
