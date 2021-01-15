dceb5900e8460d95a087aea86466cf52ba590276
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-15 15:21:12 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/GetMaximuminGeneratedArray.py
+++ LeetCode/Challenges/2021/January/Week 3/GetMaximuminGeneratedArray.py
@@ -47,11 +47,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # if n+1 > len(dp):
-        #     for i in range(len(nums), n+1):
-        #         if i%2 == 0:
-        #             nums.append(nums[i//2])
-        #         else:
-        #             nums.append(nums[i//2] + nums[i//2+1])
-        #         dp.append(max(dp[-1], nums[-1]))
-        # return dp[n]
+        if n+1 > len(dp):
+            #     for i in range(len(nums), n+1):
+            #         if i%2 == 0:
+            #             nums.append(nums[i//2])
+            #         else:
+            #             nums.append(nums[i//2] + nums[i//2+1])
+            #         dp.append(max(dp[-1], nums[-1]))
+            # return dp[n]
