6c62379ff7a568b84ed82f02057736b108ab6e0b
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-15 15:21:03 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/GetMaximuminGeneratedArray.py
+++ LeetCode/Challenges/2021/January/Week 3/GetMaximuminGeneratedArray.py
@@ -36,3 +36,22 @@
 # Constraints:
 
 # 0 <= n <= 100
+
+nums = [0, 1]
+dp = [0, 1]
+
+
+class Solution(object):
+    def getMaximumGenerated(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # if n+1 > len(dp):
+        #     for i in range(len(nums), n+1):
+        #         if i%2 == 0:
+        #             nums.append(nums[i//2])
+        #         else:
+        #             nums.append(nums[i//2] + nums[i//2+1])
+        #         dp.append(max(dp[-1], nums[-1]))
+        # return dp[n]
