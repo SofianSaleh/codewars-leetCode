c2e25ab97e3f6676c22c0cc554a98742b241fff5
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-19 12:15:36 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/LongestPalindromicSubstring.py
+++ LeetCode/Challenges/2021/January/Week 3/LongestPalindromicSubstring.py
@@ -65,6 +65,7 @@ class Solution:
             l = max(expand(s, i, i), expand(s, i, i + 1))
             if l > right - left + 1:
                 right = i + l // 2
+                left = right - l + 1
 # def longestPalindrome(self, s):
 #         left, right = -1, -2
 #         for i in xrange(len(s)):
