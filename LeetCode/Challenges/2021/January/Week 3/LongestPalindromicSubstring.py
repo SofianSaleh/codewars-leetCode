5ba14b7c44183d68d452c54616f1e6e263c7d9b1
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-19 12:13:44 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/LongestPalindromicSubstring.py
+++ LeetCode/Challenges/2021/January/Week 3/LongestPalindromicSubstring.py
@@ -1,6 +1,5 @@
 # GGiven a string s, return the longest palindromic substring in s.
 
- 
 
 # Example 1:
 
@@ -19,14 +18,13 @@
 
 # Input: s = "ac"
 # Output: "a"
- 
+
 
 # Constraints:
 
 # 1 <= s.length <= 1000
 # s consist of only digits and English letters (lower-case and/or upper-case),iven a string s, return the longest palindromic substring in s.
 
- 
 
 # Example 1:
 
@@ -45,9 +43,37 @@
 
 # Input: s = "ac"
 # Output: "a"
- 
+
 
 # Constraints:
 
 # 1 <= s.length <= 1000
 # s consist of only digits and English letters (lower-case and/or upper-case),
+
+
+class Solution:
+    def longestPalindrome(self, s):
+
+        def expand(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return (right - left + 1) - 2
+# def longestPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         def expand(s, left, right):
+#             while left >= 0 and right < len(s) and s[left] == s[right]:
+#                 left -= 1
+#                 right += 1
+#             return (right-left+1)-2
+
+#         left, right = -1, -2
+#         for i in xrange(len(s)):
+#             l = max(expand(s, i, i), expand(s, i, i+1))
+#             if l > right-left+1:
+#                 right = i+l//2
+#                 left = right-l+1
+#         return s[left:right+1] if left >= 0 else ""
