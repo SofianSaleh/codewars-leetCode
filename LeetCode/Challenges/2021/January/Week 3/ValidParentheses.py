7edd06127c889a3775544355fd6d6f34f2b34033
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-20 12:27:30 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/ValidParentheses.py
+++ LeetCode/Challenges/2021/January/Week 3/ValidParentheses.py
@@ -38,14 +38,14 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
 
-        # for c in s:
-        #     if c == '(':
-        #         stack.append(')')
-        #     elif c == '{':
-        #         stack.append('}')
-        #     elif c == '[':
-        #         stack.append(']')
-        #     elif not stack or stack.pop() != c:
-        #         return False
+        for c in s:
+            #     if c == '(':
+            #         stack.append(')')
+            #     elif c == '{':
+            #         stack.append('}')
+            #     elif c == '[':
+            #         stack.append(']')
+            #     elif not stack or stack.pop() != c:
+            #         return False
 
-        # return not stack
+            return not stack
