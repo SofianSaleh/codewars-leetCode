cd3e6d598146434245fe875e8fd46fe8c66b9840
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-21 15:05:32 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/FindtheMostCompetitiveSubsequence.py
+++ LeetCode/Challenges/2021/January/Week 3/FindtheMostCompetitiveSubsequence.py
@@ -30,5 +30,5 @@ class Solution:
             while stk and stk[-1] > x and len(stk)+(len(nums)-i) > k:
                 stk.pop()
             if len(stk) < k:
-                # stk.append(x)
+                stk.append(x)
                 # return stk
