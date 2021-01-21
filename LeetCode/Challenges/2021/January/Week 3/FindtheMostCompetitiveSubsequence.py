9cebc89ce29736f5db00a61cdc4a8fca072db6d3
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-21 15:05:20 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 3/FindtheMostCompetitiveSubsequence.py
+++ LeetCode/Challenges/2021/January/Week 3/FindtheMostCompetitiveSubsequence.py
@@ -27,8 +27,8 @@ class Solution:
     def mostCompetitive(self, nums, k):
         stk = []
         for i, x in enumerate(nums):
-            # while stk and stk[-1] > x and len(stk)+(len(nums)-i) > k:
-            # stk.pop()
-            # if len(stk) < k:
-            # stk.append(x)
-            # return stk
+            while stk and stk[-1] > x and len(stk)+(len(nums)-i) > k:
+                # stk.pop()
+                # if len(stk) < k:
+                # stk.append(x)
+                # return stk
