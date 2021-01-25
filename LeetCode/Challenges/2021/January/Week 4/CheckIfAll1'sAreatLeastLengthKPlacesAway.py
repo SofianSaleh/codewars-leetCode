333a8de2c3042587cc9da5b7eb0319178eae9ba6
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-25 13:28:44 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 4/CheckIfAll1'sAreatLeastLengthKPlacesAway.py
+++ LeetCode/Challenges/2021/January/Week 4/CheckIfAll1'sAreatLeastLengthKPlacesAway.py
@@ -35,3 +35,7 @@ class Solution:
         last = - 1
 
         for i in range(len(nums)):
+            if nums[i] == 1:
+                if last != -1:
+                    if i - last - 1 < k:
+                        return False
