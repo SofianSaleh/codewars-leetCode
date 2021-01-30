3619a8c3efd51cc47adb0f3619bc3923b849c2cf
SofianSaleh <sofiane.saleh11@gmail.com>
2021-01-30 11:18:38 +0200

    :fire:

--- LeetCode/Challenges/2021/January/Week 5/Minimize-Deviation-in-Array.py
+++ LeetCode/Challenges/2021/January/Week 5/Minimize-Deviation-in-Array.py
@@ -2,3 +2,4 @@ class Solution:
     def minimumDeviation(self, nums):
         max_heap = [-num*2 if num % 2 else -num for num in nums]
         heapq.heapify(max_heap)
+        min_elem = -max(max_heap)
