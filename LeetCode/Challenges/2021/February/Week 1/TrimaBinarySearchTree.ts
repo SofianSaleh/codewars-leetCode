929680d29fb7e593c2e9742168d26740dcb08b66
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-02 10:37:51 +0200

    :fire:

--- LeetCode/Challenges/2021/February/Week 1/TrimaBinarySearchTree.ts
+++ LeetCode/Challenges/2021/February/Week 1/TrimaBinarySearchTree.ts
@@ -48,16 +48,17 @@
  * }
  */
 
- class TreeNode {
-     val: number
-      left: TreeNode | null
-     right: TreeNode | null
-     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-         this.val = (val===undefined ? 0 : val)
-          this.left = (left===undefined ? null : left)
-          this.right = (right===undefined ? null : right)
-     }
- 
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 function trimBST(
   root: TreeNode | null,
   low: number,
