76d98cb72ab4fbd920d4583ee199f27d4bdbcd81
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-06 10:57:35 +0200

    :smile:

---
+++ LeetCode/Challenges/2021/February/Week 1/BinaryTreeRightSideView.ts
@@ -0,0 +1,50 @@
+// Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+// Example:
+
+// Input: [1,2,3,null,5,null,4]
+// Output: [1, 3, 4]
+// Explanation:
+
+//    1            <---
+//  /   \
+// 2     3         <---
+//  \     \
+//   5     4       <---
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function rightSideView(root: TreeNode | null): number[] {
+  let tr: any = new TreeNode();
+  if (root) {
+    return tr;
+  }
+  function dfs(root: TreeNode | null): any {
+    if (root) {
+      return;
+    }
+    tr.right = root.right;
+  }
+  return dfs(root);
+}
