a63cb21c46aa529f0bff5efabbc4d7eb6c537c4f
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-08 10:41:23 +0200

    :Smile:

---
+++ LeetCode/Challenges/2021/February/Week 2/PeekingIterator.js
@@ -0,0 +1,53 @@
+/**
+ * // This is the Iterator's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Iterator {
+ *      hasNext(): boolean {}
+ *
+ *      next(): number {}
+ * }
+ */
+
+// class Iterator {
+//   hasNext(): boolean {}
+
+//   next(): number {}
+// }
+var PeekingIterator = function (iterator) {
+  let it = iterator;
+  let next;
+  if (it.hasNext()) {
+    next = it.next();
+  }
+};
+
+/**
+ * @return {number}
+ */
+PeekingIterator.prototype.peek = function () {
+  return this.next();
+};
+
+/**
+ * @return {number}
+ */
+PeekingIterator.prototype.next = function () {
+  let next = this.next;
+  this.next = this.iterator.hasNext() ? this.iterator.next() : null;
+  return next;
+};
+
+/**
+ * @return {boolean}
+ */
+PeekingIterator.prototype.hasNext = function () {
+  return this.next != null;
+};
+
+/**
+ * Your PeekingIterator object will be instantiated and called as such:
+ * var obj = new PeekingIterator(iterator)
+ * var param_1 = obj.peek()
+ * var param_2 = obj.next()
+ * var param_3 = obj.hasNext()
+ */
