9edac68e5e556c9780d134d416ba74b3534ccee6
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-11 10:22:40 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 2/ValidAnagram.ts
+++ LeetCode/Challenges/2021/February/Week 2/ValidAnagram.ts
@@ -16,7 +16,7 @@
 
 // Brute force
 function isAnagram(s: string, t: string): boolean {
-  if (s.length !== t.length) return false;
+  //   if (s.length !== t.length) return false;
   let arr = s.split('');
   for (let i = 0; i < t.length; i++) {
     let index = arr.indexOf(t[i]);
