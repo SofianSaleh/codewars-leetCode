2aaef4c182f61ac934cde1698c2ddaf7d8495078
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-11 10:22:34 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 2/ValidAnagram.ts
+++ LeetCode/Challenges/2021/February/Week 2/ValidAnagram.ts
@@ -15,16 +15,16 @@
 // What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
 // Brute force
-// function isAnagram(s: string, t: string): boolean {
-//   if (s.length !== t.length) return false;
-//   let arr = s.split('');
-//   for (let i = 0; i < t.length; i++) {
-//     let index = arr.indexOf(t[i]);
-//     if (index >= 0) {
-//       arr.splice(index, 1);
-//     } else {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+  let arr = s.split('');
+  for (let i = 0; i < t.length; i++) {
+    let index = arr.indexOf(t[i]);
+    if (index >= 0) {
+      arr.splice(index, 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
