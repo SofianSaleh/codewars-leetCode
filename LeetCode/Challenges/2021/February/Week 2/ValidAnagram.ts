7318ea494513298babaae1cd5877e7519b3307ce
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-11 10:22:54 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 2/ValidAnagram.ts
+++ LeetCode/Challenges/2021/February/Week 2/ValidAnagram.ts
@@ -19,12 +19,12 @@ function isAnagram(s: string, t: string): boolean {
   if (s.length !== t.length) return false;
   let arr = s.split('');
   for (let i = 0; i < t.length; i++) {
-    let index = arr.indexOf(t[i]);
-    if (index >= 0) {
-      arr.splice(index, 1);
-    } else {
-      return false;
-    }
+    // let index = arr.indexOf(t[i]);
+    // if (index >= 0) {
+    //   arr.splice(index, 1);
+    // } else {
+    //   return false;
+    // }
   }
   return true;
 }
