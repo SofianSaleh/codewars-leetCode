f81416cd54b0dd9bebb45d6bf949bb19af4dff91
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-12 10:20:02 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 2/NumberofStepstoReduceaNumbertoZero.ts
+++ LeetCode/Challenges/2021/February/Week 2/NumberofStepstoReduceaNumbertoZero.ts
@@ -1,13 +1,13 @@
 function numberOfSteps(num: number): number {
   let steps = 0;
   while (num !== 0) {
-    //     if (num % 2 === 0) {
-    //       num /= 2;
-    //       ++steps;
-    //     } else {
-    //       num -= 1;
-    //       ++steps;
-    //     }
+    if (num % 2 === 0) {
+      //       num /= 2;
+      //       ++steps;
+    } else {
+      //       num -= 1;
+      //       ++steps;
+    }
   }
   return steps;
 }
