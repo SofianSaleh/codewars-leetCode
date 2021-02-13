72593d362a95b068229f7e5fe142497ab38c8162
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-13 11:13:26 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 2/shortest-path-in-binary-matrix.py
+++ LeetCode/Challenges/2021/February/Week 2/shortest-path-in-binary-matrix.py
@@ -12,3 +12,11 @@ class Solution(object):
             while q:
                 if 0 <= j < len(grid) and 0 <= j < len(grid[0]) and not grid[i][j]:
                     grid[i][j] = 1
+                    if i == len(grid)-1 and j == len(grid)-1:
+                        return result
+                    for d in directions:
+                        
+                        next_depth.append((i+d[0], j+d[1]))
+            q = next_depth
+
+        return -1
