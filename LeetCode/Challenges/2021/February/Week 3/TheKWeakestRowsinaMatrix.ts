2f9d5588af03b4fc4d1e0a04e7f8a3f7ed1abcff
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-15 10:36:23 +0200

    :smile:

---
+++ LeetCode/Challenges/2021/February/Week 3/TheKWeakestRowsinaMatrix.ts
@@ -0,0 +1,18 @@
+function kWeakestRows(mat: number[][], k: number): number[] {
+  let ans = [];
+
+  return ans;
+}
+
+console.log(
+  kWeakestRows(
+    [
+      [1, 1, 0, 0, 0],
+      [1, 1, 1, 1, 0],
+      [1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0],
+      [1, 1, 1, 1, 1],
+    ],
+    3
+  )
+);
