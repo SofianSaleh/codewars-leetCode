e7869dac774dd7da8bc0240312f59d21bff75189
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-16 13:16:20 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 3/LetterCasePermutation.ts
+++ LeetCode/Challenges/2021/February/Week 3/LetterCasePermutation.ts
@@ -24,29 +24,3 @@
 // S will be a string with length between 1 and 12.
 // S will consist only of letters or digits.
 function letterCasePermutation(S: string): string[] {}
-
-// class Solution {
-//   public List<String> letterCasePermutation(String S) {
-//     List<String> ans = new ArrayList<>();
-
-//     dfs(new StringBuilder(S), 0, ans);
-
-//     return ans;
-//   }
-
-//   private void dfs(StringBuilder sb, int i, List<String> ans) {
-//     if (i == sb.length()) {
-//       ans.add(sb.toString());
-//       return;
-//     }
-//     if (Character.isDigit(sb.charAt(i))) {
-//       dfs(sb, i + 1, ans);
-//       return;
-//     }
-
-//     sb.setCharAt(i, Character.toLowerCase(sb.charAt(i)));
-//     dfs(sb, i + 1, ans);
-//     sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
-//     dfs(sb, i + 1, ans);
-//   }
-// }
