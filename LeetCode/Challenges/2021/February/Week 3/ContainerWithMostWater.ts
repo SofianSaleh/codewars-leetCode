34649e9eb696d88b178df541bcc2d20f7606e7c8
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-17 15:27:40 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 3/ContainerWithMostWater.ts
+++ LeetCode/Challenges/2021/February/Week 3/ContainerWithMostWater.ts
@@ -1,1 +1,14 @@
-function maxArea(height: number[]): number {}
+function maxArea(height: number[]): number {
+  let ans = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    let area = 0;
+
+    for (let j = i + 1; j < height.length; j++) {
+      area = Math.abs(height[i] - height[j]) * (i + 1 * j + 1);
+      if (area > ans) area = ans;
+    }
+  }
+
+  return ans;
+}
