fa9e7b62e61cf672aea3e1658f12d68e4d4659da
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-18 10:50:32 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 3/ArithmeticSlices.ts
+++ LeetCode/Challenges/2021/February/Week 3/ArithmeticSlices.ts
@@ -4,6 +4,8 @@ function numberOfArithmeticSlices(A: number[]): number {
 
   for (let i = 2; i < A.length; i++) {
     if (A[i] - A[i - 1] === A[i - 1] - A[i - 2]) {
+      dp += 1;
+      ans += dp;
     }
   }
 }
