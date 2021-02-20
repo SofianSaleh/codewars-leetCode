5773e8e72fa9bd6b73889da0c893f0fe49075a2a
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-20 11:33:57 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 3/RomantoInteger.ts
+++ LeetCode/Challenges/2021/February/Week 3/RomantoInteger.ts
@@ -2,11 +2,12 @@ function romanToInt(s: string): number {
   let ans = 0;
   let roman = {
     I: 1,
-    V: 1,
-    X: 1,
-    L: 1,
-    C: 1,
-    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
   };
 
   return ans;
