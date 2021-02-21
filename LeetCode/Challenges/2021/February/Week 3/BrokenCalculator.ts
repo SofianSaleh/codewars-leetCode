f97fbcbf9233c6ce04692e213bd4573f3b2ac21e
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-21 16:06:09 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 3/BrokenCalculator.ts
+++ LeetCode/Challenges/2021/February/Week 3/BrokenCalculator.ts
@@ -2,7 +2,7 @@ function brokenCalc(X: number, Y: number): number {
   let ops = 0;
   while (X < Y) {
     if (Y % 2 === 0) {
-      Y = Math.floor(Y / 2);
+      Y = Y / 2;
     } else {
       Y += 1;
       ops += 1;
