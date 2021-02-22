182217c4b6a124c57c3a8e8c04621c5091062811
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-22 10:07:24 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 4/LongestWordinDictionarythroughDeleting.ts
+++ LeetCode/Challenges/2021/February/Week 4/LongestWordinDictionarythroughDeleting.ts
@@ -2,5 +2,11 @@ function findLongestWord(s: string, d: string[]): string {
   let ans = '';
 
   for (const word of d) {
+    let i = 0;
+
+    for (const c in s) {
+      if (i < word.length && c === word[i]) {
+      }
+    }
   }
 }
