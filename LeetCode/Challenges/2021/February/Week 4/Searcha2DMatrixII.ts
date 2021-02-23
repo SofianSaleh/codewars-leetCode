38bf5caaf2365f57dd8c96a67637f9b1445987c2
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-23 10:40:39 +0200

    :sad:

--- LeetCode/Challenges/2021/February/Week 4/Searcha2DMatrixII.ts
+++ LeetCode/Challenges/2021/February/Week 4/Searcha2DMatrixII.ts
@@ -1,8 +1,8 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
   for (const mat of matrix) {
-    //     if(mat.indexOf(target) !== -1){
-    //         return true
-    //     }
+    if (mat.indexOf(target) !== -1) {
+      //         return true
+    }
   }
   return false;
 }
