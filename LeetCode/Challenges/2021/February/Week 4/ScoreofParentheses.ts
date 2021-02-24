cbd826ec31edd4a40a22fb9187262915f3c53c59
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-24 10:35:36 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 4/ScoreofParentheses.ts
+++ LeetCode/Challenges/2021/February/Week 4/ScoreofParentheses.ts
@@ -2,5 +2,9 @@ function scoreOfParentheses(S: string): number {
   let ans: number = 0;
   let layer: number = 0;
 
+  for (let i = 0; i + 1 < S.length; i++) {
+    let a = S.charAt(i);
+  }
+
   return ans;
 }
