b892825880d8763ed3fab923369ec5fdde746f40
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-25 12:32:17 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 4/ShortestUnsortedContinuousSubarray.ts
+++ LeetCode/Challenges/2021/February/Week 4/ShortestUnsortedContinuousSubarray.ts
@@ -1,13 +1,19 @@
 function findUnsortedSubarray(nums: number[]): number {
   let n: number = nums.length;
   let mini: number = Infinity;
-  let max: number = -Infinity;
+  let maxi: number = -Infinity;
   let flagDecrease: boolean = false;
   let flagIncrease: boolean = false;
 
+  //   To find if there is a decrease in the order
   for (let i = 1; i < n; i++) {
     if (nums[i] < nums[i - 1]) flagDecrease = true;
     if (flagDecrease) mini = Math.min(nums[i], mini);
+  }
+  //   To find if there is an increase in the order
+  for (let i = n - 2; i >= 0; i--) {
+    if (nums[i] < nums[i + 1]) flagIncrease = true;
+    if (flagIncrease) mini = Math.max(nums[i], maxi);
   }
 
   return 1;
