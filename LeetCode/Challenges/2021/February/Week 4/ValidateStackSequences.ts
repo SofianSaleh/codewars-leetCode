1d38d13677d653f6239d8ae015b3734e04a9a4fd
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-26 10:46:32 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 4/ValidateStackSequences.ts
+++ LeetCode/Challenges/2021/February/Week 4/ValidateStackSequences.ts
@@ -1,1 +1,10 @@
-function validateStackSequences(pushed: number[], popped: number[]): boolean {}
+function validateStackSequences(pushed: number[], popped: number[]): boolean {
+    let stack: number[] = []
+    let j:number = 0
+
+    for(const x: number of pushed){
+        stack.push(x)
+
+        while( stack && j < popped.length && stack[stack.length - 1] === popped[j])
+    }
+}
