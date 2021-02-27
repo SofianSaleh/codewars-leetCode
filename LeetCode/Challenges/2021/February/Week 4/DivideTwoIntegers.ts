7124d017719255dac8e9615bbb1021821e3b1836
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-27 11:03:29 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 4/DivideTwoIntegers.ts
+++ LeetCode/Challenges/2021/February/Week 4/DivideTwoIntegers.ts
@@ -1,1 +1,3 @@
-function divide(dividend: number, divisor: number): number {}
+function divide(dividend: number, divisor: number): number {
+  if (dividend === 2 ** 31 && divisor > -1) return 2 ** 31 - 1;
+}
