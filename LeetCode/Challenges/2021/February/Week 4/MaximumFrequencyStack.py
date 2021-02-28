84abb889aabc167127754b9d863efc99bbdfda3f
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-28 16:27:19 +0200

    :smile:

--- LeetCode/Challenges/2021/February/Week 4/MaximumFrequencyStack.py
+++ LeetCode/Challenges/2021/February/Week 4/MaximumFrequencyStack.py
@@ -11,8 +11,8 @@ class FreqStack(object):
         :rtype: None
         """
         self.count[x] += 1
-        self.maxFreq = max(self.maxFreq, self.count[x])
-        self.countToStack[self.count[x]].append(x)
+        # self.maxFreq = max(self.maxFreq, self.count[x])
+        # self.countToStack[self.count[x]].append(x)
         
 
     def pop(self):
