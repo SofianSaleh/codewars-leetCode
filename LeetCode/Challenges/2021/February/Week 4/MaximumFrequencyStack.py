77eb4427a29df1a0c9afce4ea7f8be20c7a4e52c
SofianSaleh <sofiane.saleh11@gmail.com>
2021-02-28 16:26:05 +0200

    :smile:

---
+++ LeetCode/Challenges/2021/February/Week 4/MaximumFrequencyStack.py
@@ -0,0 +1,17 @@
+class FreqStack(object):
+
+    def __init__(self):
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        
