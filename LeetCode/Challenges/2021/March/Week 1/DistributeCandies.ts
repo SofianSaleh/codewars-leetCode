68012fd502c3fb864b5924bf073f1fc4a54ad8f9
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-01 13:10:47 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 1/DistributeCandies.ts
+++ LeetCode/Challenges/2021/March/Week 1/DistributeCandies.ts
@@ -1,5 +1,5 @@
 function distributeCandies(candyType: number[]): number {
   let allowed = candyType.length / 2;
-  //   let candySet = new Set(candyType);
+  let candySet = new Set(candyType);
   //   return Math.min(allowed, candySet.size;
 }
