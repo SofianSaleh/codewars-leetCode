4fa61eea57fb3fb27b648bcbd31fae817ab76d01
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-02 11:08:52 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 1/SetMismatch.ts
+++ LeetCode/Challenges/2021/March/Week 1/SetMismatch.ts
@@ -1,6 +1,8 @@
 function findErrorNums(nums: number[]): number[] {
+  let duplicate;
   for (const num of nums) {
     if (nums[Math.abs(num) - 1] < 0) {
+      duplicate = Math.abs(num);
     } else {
     }
   }
