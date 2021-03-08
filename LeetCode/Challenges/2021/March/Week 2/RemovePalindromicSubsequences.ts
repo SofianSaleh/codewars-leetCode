bf748fa79f84b01a449773211c05a9804da1c3a2
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-08 13:53:02 +0200

    :smile:

--- LeetCode/Challenges/2021/March/Week 2/RemovePalindromicSubsequences.ts
+++ LeetCode/Challenges/2021/March/Week 2/RemovePalindromicSubsequences.ts
@@ -1,5 +1,5 @@
 function removePalindromeSub(s: string): number {
-  //   if (!s) return 0;
+  if (!s) return 0;
   //   for (let i = 0, j = s.length - 1; i < j; i++, j--)
   //     if (s.charAt(i) !== s.charAt(j)) return 2;
   //   return 1;
