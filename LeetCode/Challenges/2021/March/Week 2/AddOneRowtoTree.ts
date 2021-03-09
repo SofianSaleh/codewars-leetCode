a28135d00ae8a17cd04c2f09e7ba8b8b1fd519b3
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-09 14:18:58 +0200

    :smile:

--- LeetCode/Challenges/2021/March/Week 2/AddOneRowtoTree.ts
+++ LeetCode/Challenges/2021/March/Week 2/AddOneRowtoTree.ts
@@ -11,9 +11,23 @@
  *     }
  * }
  */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
 function addOneRow(
   root: TreeNode | null,
   v: number,
   d: number
-): TreeNode | null {}
+): TreeNode | null {
+  if (d === 1) {
+    let newRoot: TreeNode = new TreeNode(v);
+  }
+}
