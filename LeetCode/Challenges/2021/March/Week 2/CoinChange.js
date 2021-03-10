7168a36ca773d07029c8c8053c1d7c8e39031e09
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-10 12:06:09 +0200

    :smile:

--- LeetCode/Challenges/2021/March/Week 2/CoinChange.js
+++ LeetCode/Challenges/2021/March/Week 2/CoinChange.js
@@ -1,6 +1,8 @@
 function coinChange(coins, amount) {
   let dp = [];
-  dp.fill(1, dp.length, amount + 1);
+
+  dp.fill(amount + 1, 1, amount + 1);
+  dp[0] = 0;
   console.log(dp);
   for (const coin of coins) {
     for (let i = coin; i <= amount; ++i) {
@@ -11,19 +13,6 @@ function coinChange(coins, amount) {
   return dp[amount] == amount + 1 ? -1 : dp[amount];
 }
 
-// class Solution {
-//   public int coinChange(int[] coins, int amount) {
-//     // dp[i] := fewest # of coins to make up i
-//     int[] dp = new int[amount + 1];
-//     Arrays.fill(dp, 1, dp.length, amount + 1);
-
-//     for (final int coin : coins)
-//       for (int i = coin; i <= amount; ++i)
-//         dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-
-//     r
-//   }
-// }
 // console.log(coinChange([1, 2, 5], 11));
 console.log(coinChange([1, 2, 5, 10], 18));
 // console.log(coinChange([2], 3));
