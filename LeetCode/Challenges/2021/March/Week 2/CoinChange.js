07f03e3018cd87dc6e56bad841a9be5955ca45fd
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-11 12:26:14 +0200

    :smile:

--- LeetCode/Challenges/2021/March/Week 2/CoinChange.js
+++ LeetCode/Challenges/2021/March/Week 2/CoinChange.js
@@ -1,17 +1,19 @@
 function coinChange(coins, amount) {
   let dp = [];
-  dp.fill(amount + 1, 1, amount + 1);
-  console.log(dp);
-  dp[0] = 0;
-  for (const coin of coins) {
-    for (let i = coin; i <= amount; ++i) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-    }
-  }
+  // dp[0] = 0;
+  // for (let i = 1; i < amount + 1; i++) {
+  //   dp.push(amount + 1);
+  // }
 
-  return dp[amount] == amount + 1 ? -1 : dp[amount];
+  // for (const coin of coins) {
+  //   for (let i = coin; i <= amount; ++i) {
+  //     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+  //   }
+  // }
+
+  // return dp[amount] == amount + 1 ? -1 : dp[amount];
 }
 
-// console.log(coinChange([1, 2, 5], 11));
-console.log(coinChange([1, 2, 5, 10], 18));
+console.log(coinChange([1, 2, 5], 11));
+// console.log(coinChange([1, 2, 5, 10], 18));
 // console.log(coinChange([2], 3));
