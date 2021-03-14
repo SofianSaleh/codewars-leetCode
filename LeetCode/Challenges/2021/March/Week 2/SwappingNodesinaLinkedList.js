e82156ce91b3484204e38604111428242e57f88c
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-14 14:05:55 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
+++ LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
@@ -28,35 +28,47 @@ let x = new ListNode(
  * @return {ListNode}
  */
 var swapNodes = function (head, k) {
-  let start = head;
-  let first;
 
-  let i = 0;
-  let j = 0;
-  let l = 0;
-
-  while (start) {
-    i++;
-    if (i === k) {
-      first = start;
-    }
-    start = start.next;
-  }
-  start = head;
-  console.log(i);
-  i = i - k + 1;
-  while (start) {
-    j++;
-    if (j === i) {
-      second = start;
-    }
-    start = start.next;
-  }
-  start = head;
-  while (start) {
-    start.next;
-  }
-  console.log(i, first, second);
+    if (x == y)
+        return 
 };
 
 console.log(swapNodes(x, 2));
+
+
+def swapNodes(self, x, y): 
+  
+  
+        # Search for x (keep track of prevX and CurrX) 
+        prevX = None
+        currX = self.head 
+        while currX != None and currX.data != x: 
+            prevX = currX 
+            currX = currX.next
+  
+        # Search for y (keep track of prevY and currY) 
+        prevY = None
+        currY = self.head 
+        while currY != None and currY.data != y: 
+            prevY = currY 
+            currY = currY.next
+  
+        # If either x or y is not present, nothing to do 
+        if currX == None or currY == None: 
+            return 
+        # If x is not head of linked list 
+        if prevX != None: 
+            prevX.next = currY 
+        else: #make y the new head 
+            self.head = currY 
+  
+        # If y is not head of linked list 
+        if prevY != None: 
+            prevY.next = currX 
+        else: # make x the new head 
+            self.head = currX 
+  
+        # Swap next pointers 
+        temp = currX.next
+        currX.next = currY.next
+        currY.next = temp 
