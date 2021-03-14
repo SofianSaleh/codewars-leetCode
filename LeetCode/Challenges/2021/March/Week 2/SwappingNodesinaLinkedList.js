207c36d62d3bd6a60f3693968dfc94ebeacee6d6
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-14 13:50:26 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
+++ LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
@@ -5,9 +5,11 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 /**
  * @param {ListNode} head
