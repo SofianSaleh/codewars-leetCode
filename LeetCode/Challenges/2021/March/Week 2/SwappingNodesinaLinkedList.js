1543fe9069ae48b2bb888123d52c6a8b83a33a33
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-14 14:00:15 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
+++ LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
@@ -11,11 +11,46 @@ class ListNode {
     this.next = next === undefined ? null : next;
   }
 }
+
+let x = new ListNode(
+  1,
+  new ListNode(
+    2,
+    new ListNode(
+      3,
+      new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7))))
+    )
+  )
+);
 /**
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
  */
 var swapNodes = function (head, k) {
-  console.log(head.length);
+  let start = head;
+  let first;
+  let i = 0;
+  while (start) {
+    i++;
+    if (i === k) {
+      first = start;
+    }
+    start = start.next;
+  }
+  start = head;
+  console.log(i);
+  let j = 0;
+  i = i - k + 1;
+  while (start) {
+    j++;
+    if (j === i) {
+      second = start;
+    }
+    start = start.next;
+  }
+  while
+  console.log(i, first, second);
 };
+
+console.log(swapNodes(x, 2));
