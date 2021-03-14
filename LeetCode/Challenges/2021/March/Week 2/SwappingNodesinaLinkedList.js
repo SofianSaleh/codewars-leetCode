84bc1a6089300f65fcc8a85c425a6ecc5ef434e7
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-14 13:50:20 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
+++ LeetCode/Challenges/2021/March/Week 2/SwappingNodesinaLinkedList.js
@@ -5,6 +5,10 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
 /**
  * @param {ListNode} head
  * @param {number} k
