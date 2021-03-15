4cdcd09913bfddfc26ef0d50b6ae41e64ca406ad
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-15 17:30:50 +0200

    :fire:

---
+++ LeetCode/Challenges/2021/March/Week 3/EncodeandDecodeTinyURL.ts
@@ -0,0 +1,19 @@
+/**
+ * Encodes a URL to a shortened URL.
+ */
+
+const url = {};
+const ALPHANUMERIC =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const url = {};
+function encode(longUrl: string): string {}
+
+/**
+ * Decodes a shortened URL to its original URL.
+ */
+function decode(shortUrl: string): string {}
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
