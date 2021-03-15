56b62f7ea23000bc5689828cff4323df4480637e
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-15 17:33:55 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 3/EncodeandDecodeTinyURL.ts
+++ LeetCode/Challenges/2021/March/Week 3/EncodeandDecodeTinyURL.ts
@@ -7,8 +7,24 @@ const ALPHANUMERIC: string =
   'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 const prefix: string = 'http://tinyurl.com/';
 
-function encode(longUrl: string): string {}
+function encode(longUrl: string): string {
+    // ! generate Id
+}
 
+
+/**
+ * 
+ * @param n number
+ */
+
+const createdIdSize( n: number) : string {
+    let i: number = n
+    let id : string = ''
+
+    while(i-- > 0){
+        id += ALPHANUMERIC[(Math.random() * ALPHANUMERIC.length) | 0]
+    }
+}
 /**
  * Decodes a shortened URL to its original URL.
  */
