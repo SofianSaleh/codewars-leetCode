7c4a8a9526c32a79213e5bb861d2b7326a03eb24
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-16 20:31:49 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 3/BestTimetoBuyandSellStockwithTransactionFee.ts
+++ LeetCode/Challenges/2021/March/Week 3/BestTimetoBuyandSellStockwithTransactionFee.ts
@@ -1,6 +1,6 @@
 function maxProfit(prices: number[], fee: number): number {
-  let sell = 0;
-  let hold = -Infinity;
+  let sell: number = 0;
+  let hold: number = Infinity * -1;
 
   for (const price in prices) {
     sell = Math.max(sell, hold + price);
