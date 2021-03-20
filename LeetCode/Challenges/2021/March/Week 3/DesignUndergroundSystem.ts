4ad1bfedafa986ab741b98d134cf408f8f18c583
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-20 16:07:51 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 3/DesignUndergroundSystem.ts
+++ LeetCode/Challenges/2021/March/Week 3/DesignUndergroundSystem.ts
@@ -1,22 +1,3 @@
-class UndergroundSystem {
-  obj: {};
-  constructor() {
-   this.checkIns = {}
-    this.checkOuts = defaultdict(lambda: [0, 0]);
-  }
-
-  checkIn(id: number, stationName: string, t: number): void {}
-
-  checkOut(id: number, stationName: string, t: number): void {}
-
-  getAverageTime(startStation: string, endStation: string): number {}
-}
-{
-  stationdep: {
-    time: 0;
-    stationEnd: [id, time];
-  }
-}
 /**
  * Your UndergroundSystem object will be instantiated and called as such:
  * var obj = new UndergroundSystem()
