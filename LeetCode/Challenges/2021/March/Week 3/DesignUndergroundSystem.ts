3666f180ad855551aa0e45e5addd8be0a5c90b4a
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-20 16:05:00 +0200

    :fire:

---
+++ LeetCode/Challenges/2021/March/Week 3/DesignUndergroundSystem.ts
@@ -0,0 +1,20 @@
+class UndergroundSystem {
+  obj: {};
+  constructor() {
+    this.obj = {};
+  }
+
+  checkIn(id: number, stationName: string, t: number): void {}
+
+  checkOut(id: number, stationName: string, t: number): void {}
+
+  getAverageTime(startStation: string, endStation: string): number {}
+}
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
