9aaf13c7ae66f364e16420c0be2278e4c7265616
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-23 17:51:52 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 4/3SumWithMultiplicity.js
+++ LeetCode/Challenges/2021/March/Week 4/3SumWithMultiplicity.js
@@ -1,27 +1,35 @@
-// # class Solution {
-// #  public:
-// #   int threeSumMulti(vector<int>& A, int target) {
-// #     const int kMod = 1e9 + 7;
+// # cl/**
+ * @param {number[]} arr
+* @param {number} target
+* @return {number}
+*/
+var threeSumMulti = function(arr, target) {
+    const kMod = 1e9 + 7;
+    let ans = 0;
+    let count = {};
+    for (const a of  A)
+      ++count[a];
+   
+};
+class Solution {
+ public:
+  int threeSumMulti(vector<int>& A, int target) {
 
-// #     long ans = 0;
 
-// #     unordered_map<int, long> count;
-// #     for (int a : A)
-// #       ++count[a];
 
-// #     for (const auto& [i, x] : count)
-// #       for (const auto& [j, y] : count) {
-// #         int k = target - i - j;
-// #         if (!count.count(k))
-// #           continue;
-// #         if (i == j && j == k)
-// #           ans += x * (x - 1) * (x - 2) / 6;
-// #         else if (i == j && j != k)
-// #           ans += x * (x - 1) / 2 * count[k];
-// #         else if (i < j && j < k)
-// #           ans += x * y * count[k];
-// #       }
+    for (const auto& [i, x] : count)
+      for (const auto& [j, y] : count) {
+        int k = target - i - j;
+        if (!count.count(k))
+          continue;
+        if (i == j && j == k)
+          ans += x * (x - 1) * (x - 2) / 6;
+        else if (i == j && j != k)
+          ans += x * (x - 1) / 2 * count[k];
+        else if (i < j && j < k)
+          ans += x * y * count[k];
+      }
 
-// #     return ans % kMod;
-// #   }
-// # };
+    return ans % kMod;
+  }
+};
