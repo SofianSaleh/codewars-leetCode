2a3fa4c1f4d7b371bf4cbf2a57302714b7246fdf
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-24 12:30:01 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 4/AdvantageShuffle.py
+++ LeetCode/Challenges/2021/March/Week 4/AdvantageShuffle.py
@@ -5,9 +5,9 @@ class Solution:
   def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
     sl = SortedList(A)
 
-    for i, b in enumerate(B):
-      index = 0 if sl[-1] <= b else sl.bisect_right(b)
-      A[i] = sl[index]
-      del sl[index]
+    # for i, b in enumerate(B):
+    #   index = 0 if sl[-1] <= b else sl.bisect_right(b)
+    #   A[i] = sl[index]
+    #   del sl[index]
 
     return A
