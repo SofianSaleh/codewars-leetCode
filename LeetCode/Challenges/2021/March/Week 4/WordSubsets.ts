6713bb5634e7cac3491cbfb1da89d02e58bc459e
SofianSaleh <sofiane.saleh11@gmail.com>
2021-03-26 12:03:53 +0200

    :fire:

--- LeetCode/Challenges/2021/March/Week 4/WordSubsets.ts
+++ LeetCode/Challenges/2021/March/Week 4/WordSubsets.ts
@@ -1,17 +1,20 @@
-[[function wordSubsets(A: string[], B: string[]): string[] {
-    let count = 0
-    let ans = []
-    for(const word of A){
-        for (let i = 0; i < B.length; i++) {
-            if(word.includes(B[i])){
-                count ++
-            }
-            
-        }
-        if(count == B.length - 1){
-            ans.push(word)
-        }
-        count = 0
+function wordSubsets(A: string[], B: string[]): string[] {
+  let count = 0;
+  let ans = [];
+  for (const word of A) {
+    for (let i = 0; i < B.length; i++) {
+      console.log(word, B[i]);
+      if (word.split('').includes(B[i])) {
+        count++;
+      }
     }
-    return ans
+    if (count == B.length - 1) {
+      ans.push(word);
+    }
+    count = 0;
+  }
+  return ans;
 }
+console.log(
+  wordSubsets(['amazon', 'apple', 'facebook', 'google', 'leetcode'], ['e', 'o'])
+);
