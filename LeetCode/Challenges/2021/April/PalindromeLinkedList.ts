7e107e1defda1cf918b8f7af99c991128458d5d4
SofianSaleh <sofiane.saleh11@gmail.com>
2021-04-01 16:41:49 +0200

    :fire:

---
+++ LeetCode/Challenges/2021/April/PalindromeLinkedList.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+  function reverseLinkedList(head: ListNode): ListNode;
+}
