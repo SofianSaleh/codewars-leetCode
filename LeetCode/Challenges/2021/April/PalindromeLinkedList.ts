82bdca606849c9b0ea2cd405aafe65eb21dd2ddf
SofianSaleh <sofiane.saleh11@gmail.com>
2021-04-01 16:44:57 +0200

    :fire:

--- LeetCode/Challenges/2021/April/PalindromeLinkedList.ts
+++ LeetCode/Challenges/2021/April/PalindromeLinkedList.ts
@@ -19,5 +19,15 @@ class ListNode {
 }
 
 function isPalindrome(head: ListNode | null): boolean {
-  function reverseLinkedList(head: ListNode): ListNode;
+  function reverseLinkedList(head: ListNode): ListNode {
+    let prev = null;
+    let curr = head;
+
+    while (curr) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+  }
 }
